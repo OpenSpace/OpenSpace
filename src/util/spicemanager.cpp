3ae37d099187f85232df1cb1791e26c743ce7e84
Alexander Bock <alexander.bock@liu.se>
2014-09-14 23:53:41 +0200

    More compile fixes for Jenkins

--- src/util/spicemanager.cpp
+++ src/util/spicemanager.cpp
@@ -21,9 +21,11 @@
 * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE  *
 * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                         *
 ****************************************************************************************/
+
 #include <stdio.h>
 #include <iostream>
 #include <cassert>
+#include <cstring>
 
 #ifdef WIN32
 #include <Windows.h>
