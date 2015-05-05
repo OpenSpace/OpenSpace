dfca3152930820ad62c5865b67f8f197eeb797bc
Alexander Bock <alexander.bock@liu.se>
2015-05-05 17:29:12 +0200

    Fixing issue with linux

--- src/util/labelparser.cpp
+++ src/util/labelparser.cpp
@@ -22,7 +22,7 @@
 * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                         *
 ****************************************************************************************/
 
-#include <openspace/util/ImageSequencer2.h>
+#include <openspace/util/imagesequencer2.h>
 #include <ghoul/logging/logmanager.h>
 #include <ghoul/filesystem/filesystem.h>
 #include <ghoul/filesystem/directory.h>
