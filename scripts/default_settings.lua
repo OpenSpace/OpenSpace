cf4afc015925491ba341a86056fcb23b039f30b3
Alexander Bock <alexander.bock@liu.se>
2015-05-14 01:13:14 +0200

    Set default coordinate system to Jupiter to agree with the default time

--- scripts/default_settings.lua
+++ scripts/default_settings.lua
@@ -11,4 +11,6 @@ openspace.setPropertyValue("Constellation Bounds.renderable.enabled", false)
 openspace.setPropertyValue("MilkyWay.renderable.transparency", 0.75)
 openspace.setPropertyValue("MilkyWay.renderable.segments", 50)
 
+openspace.changeCoordinateSystem("Jupiter")
+
 openspace.printInfo("Done setting default values")
