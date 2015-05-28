f9d4e0e1f1af97d8bba68fe675c78ef03ff8246a
Alexander Bock <alexander.bock@liu.se>
2015-05-28 22:25:58 +0200

    Clean test main.cpp

--- tests/main.cpp
+++ tests/main.cpp
@@ -55,25 +55,6 @@ int main(int argc, char** argv) {
     std::vector<std::string> args;
     openspace::OpenSpaceEngine::create(argc, argv, args);
 
-    
-    //LogManager::initialize(LogManager::LogLevel::Debug);
-    //LogMgr.addLog(new ConsoleLog);
-
-    //FileSystem::initialize();
-    //std::string configurationFilePath = "";
-    //LDEBUG("Finding configuration");
-    //if (!openspace::OpenSpaceEngine::findConfiguration(configurationFilePath)) {
-    //    LFATAL("Could not find OpenSpace configuration file!");
-    //    assert(false);
-    //}
-	////LINFO("Configuration file found: " << FileSys.absolutePath(configurationFilePath));
-
-	//openspace::ConfigurationManager manager;
-	//manager.loadFromFile(configurationFilePath);
-
-    
-    //openspace::FactoryManager::initialize();
-    
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
