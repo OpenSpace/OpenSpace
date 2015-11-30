999f198aa33d791d0cb6d8e47a7d64c3ab6ff899
Alexander Bock <alexander.bock@liu.se>
2015-11-30 15:01:02 -0500

    Updated tinyobjloader and applied the changes

--- modules/base/rendering/wavefrontgeometry.cpp
+++ modules/base/rendering/wavefrontgeometry.cpp
@@ -53,9 +53,10 @@ void WavefrontGeometry::deinitialize() {
 bool WavefrontGeometry::loadModel(const std::string& filename) {
 	std::vector<tinyobj::shape_t> shapes;
 	std::vector<tinyobj::material_t> materials;
-    std::string err = tinyobj::LoadObj(shapes, materials, filename.c_str(), filename.c_str());
+    std::string err;
+    bool success = tinyobj::LoadObj(shapes, materials, err, filename.c_str(), filename.c_str());
 
-    if (!err.empty()) {
+    if (!success) {
         LERROR(err);
         return false;
     }
