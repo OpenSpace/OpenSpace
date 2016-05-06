4188aebb5ed3aa4a178885ed20364db8e034fd25
Alexander Bock <alexander.bock@liu.se>
2016-05-06 11:23:40 +0200

    Cleanup of data/scene directory

--- data/scene/volumegl/sampler.glsl
+++
@@ -1,5 +0,0 @@
-vec4 sampleVolume1(inout vec4 finalColor, vec3 position) {
-	float intensity = texture(volume1, position).x;
-	return texture(transferFunction1, intensity);
-	//blendStep(finalColor, color, stepSize);
-}
