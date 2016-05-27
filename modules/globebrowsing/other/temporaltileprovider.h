b8ab68647ba1eea478dd2fe84f91fed1093d013f
Erik Broberg <Erik Broberg>
2016-05-27 17:01:18 -0400

    Added unimplemented class TemporalTileProvider

---
+++ modules/globebrowsing/other/temporaltileprovider.h
@@ -0,0 +1,50 @@
+/*****************************************************************************************
+*                                                                                       *
+* OpenSpace                                                                             *
+*                                                                                       *
+* Copyright (c) 2014-2016                                                               *
+*                                                                                       *
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this  *
+* software and associated documentation files (the "Software"), to deal in the Software *
+* without restriction, including without limitation the rights to use, copy, modify,    *
+* merge, publish, distribute, sublicense, and/or sell copies of the Software, and to    *
+* permit persons to whom the Software is furnished to do so, subject to the following   *
+* conditions:                                                                           *
+*                                                                                       *
+* The above copyright notice and this permission notice shall be included in all copies *
+* or substantial portions of the Software.                                              *
+*                                                                                       *
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,   *
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A         *
+* PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT    *
+* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF  *
+* CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE  *
+* OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                         *
+****************************************************************************************/
+
+#ifndef __TEMPORAL_TILE_PROVIDER_H__
+#define __TEMPORAL_TILE_PROVIDER_H__
+
+
+#include <ghoul/opengl/texture.h>
+
+#include <modules/globebrowsing/geodetics/geodetic2.h>
+#include <modules/globebrowsing/other/tileprovider.h>
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//									TILE PROVIDER									    //
+//////////////////////////////////////////////////////////////////////////////////////////
+
+namespace openspace {
+   
+    
+    class TemporalTileProvider {
+        
+    };
+
+}  // namespace openspace
+
+
+
+
+#endif  // __TEMPORAL_TILE_PROVIDER_H__
