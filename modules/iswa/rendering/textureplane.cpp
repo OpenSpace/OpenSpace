fccf05a02620f4db0fc5d05429063f4a64e10571
Michael Nilsson <michaelnilsson90@hotmail.com>
2016-05-27 09:57:11 -0400

    uncommented important code

--- modules/iswa/rendering/textureplane.cpp
+++ modules/iswa/rendering/textureplane.cpp
@@ -1,26 +1,26 @@
-// /*****************************************************************************************
-//  *                                                                                       *
-//  * OpenSpace                                                                             *
-//  *                                                                                       *
-//  * Copyright (c) 2014-2016                                                               *
-//  *                                                                                       *
-//  * Permission is hereby granted, free of charge, to any person obtaining a copy of this  *
-//  * software and associated documentation files (the "Software"), to deal in the Software *
-//  * without restriction, including without limitation the rights to use, copy, modify,    *
-//  * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to    *
-//  * permit persons to whom the Software is furnished to do so, subject to the following   *
-//  * conditions:                                                                           *
-//  *                                                                                       *
-//  * The above copyright notice and this permission notice shall be included in all copies *
-//  * or substantial portions of the Software.                                              *
-//  *                                                                                       *
-//  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,   *
-//  * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A         *
-//  * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT    *
-//  * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF  *
-//  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE  *
-//  * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                         *
-//  ****************************************************************************************/
+/*****************************************************************************************
+ *                                                                                       *
+ * OpenSpace                                                                             *
+ *                                                                                       *
+ * Copyright (c) 2014-2016                                                               *
+ *                                                                                       *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this  *
+ * software and associated documentation files (the "Software"), to deal in the Software *
+ * without restriction, including without limitation the rights to use, copy, modify,    *
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to    *
+ * permit persons to whom the Software is furnished to do so, subject to the following   *
+ * conditions:                                                                           *
+ *                                                                                       *
+ * The above copyright notice and this permission notice shall be included in all copies *
+ * or substantial portions of the Software.                                              *
+ *                                                                                       *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,   *
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A         *
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT    *
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF  *
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE  *
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                         *
+ ****************************************************************************************/
 #include <modules/iswa/rendering/textureplane.h>
 #include <openspace/engine/openspaceengine.h>
 #include <openspace/rendering/renderengine.h>
@@ -74,20 +74,18 @@ bool TexturePlane::loadTexture() {
                                                         (void*) imageFile.buffer,
                                                         imageFile.size, 
                                                         imageFile.format);
-    // delete[] (char*) imageFile.buffer;
-    // if (texture) {
-    //     LDEBUG("Loaded texture from image iswa cygnet with id: '" << _data->id << "'");
 
-    //     texture->uploadTexture();
-    //     // Textures of planets looks much smoother with AnisotropicMipMap rather than linear
-    //     texture->setFilter(ghoul::opengl::Texture::FilterMode::Linear);
-    //     _textures[0]  = std::move(texture);
-    // }
+    if (texture) {
+        LDEBUG("Loaded texture from image iswa cygnet with id: '" << _data->id << "'");
 
-    // return false;
+        texture->uploadTexture();
+        // Textures of planets looks much smoother with AnisotropicMipMap rather than linear
+        texture->setFilter(ghoul::opengl::Texture::FilterMode::Linear);
+        _textures[0]  = std::move(texture);
+    }
+
+    return false;
 }
-
-
 
 bool TexturePlane::updateTexture(){
 
