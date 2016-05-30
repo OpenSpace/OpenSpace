fde537681fe18179bc46c1d9018d454d9d67a734
Alexander Bock <alexander.bock@liu.se>
2016-05-30 15:26:47 +0200

    Some work on fixing 67P projections for all available times

--- scripts/bind_keys_rosetta.lua
+++ scripts/bind_keys_rosetta.lua
@@ -9,15 +9,15 @@ helper.setCommonKeys()
 
 helper.setDeltaTimeKeys({
     1, 5, 10, 20, 40, 90, 360, 720, 2880, 14400,
-    28800, 57600, 115200, 230400, 460800
+    28800, 57600, 115200, 230400, 460800, 921600, 1843200, 3686400, 7372800, 14745600
 })
 
 openspace.bindKey("a", "openspace.setPropertyValue('Interaction.origin', '67P')")
 openspace.bindKey("s", "openspace.setPropertyValue('Interaction.origin', 'Rosetta')")
 
-openspace.bindKey("F5", "openspace.setPropertyValue('Interaction.coordinateSystem', 'Sun'); openspace.printInfo('Changing Viewpoint to Sun');");
+-- openspace.bindKey("F5", "openspace.setPropertyValue('Interaction.coordinateSystem', 'Sun'); openspace.printInfo('Changing Viewpoint to Sun');");
 openspace.bindKey("F6", "openspace.setPropertyValue('Interaction.coordinateSystem', '67P'); openspace.printInfo('Changing Viewpoint to 67P');");
-openspace.bindKey("F8", "openspace.setPropertyValue('67P.renderable.clearAllProjections', true");
+openspace.bindKey("F8", "openspace.setPropertyValue('67P.renderable.clearAllProjections', true);");
 
 openspace.bindKey("i", helper.renderable.toggle('ImagePlaneRosetta'))
 openspace.bindKey("q", helper.renderable.toggle('SunMarker'))
