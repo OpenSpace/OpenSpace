8b4e6e855f891aa2023a875c3a4c0f24004d2e28
Erik Broberg <erikbroberg90@gmail.com>
2016-08-26 17:41:44 -0400

    Mkae DeltaTimeKeys map to values relating to minutes, hours, days, years, etc

--- scripts/bind_keys.lua
+++ scripts/bind_keys.lua
@@ -6,10 +6,19 @@ dofile(openspace.absPath('${SCRIPTS}/common.lua'))
 openspace.clearKeys()
 helper.setCommonKeys()
 helper.setDeltaTimeKeys({
-    1, 5, 10, 20, 40, 60, 120, 360, 720, 1440,
-    2880, 5760, 11520, 23040, 46080, 92160, 184320, 368640, 737280, 1474560,
-    2949120, 5898240, 11796480, 23592960, 47185920, 94371840, 188743680, 377487360
+--  1			2		3		4		5		6		7		8		9		0
+--------------------------------------------------------------------------------------------------------------------------
+--  1s            2s 		5s		10s		30s		1m  		2m   		5m   		10m  		30 min
+    1, 		2, 		5, 		10, 		30, 		60, 		120, 		300, 		600, 		1800,
+
+--  1h    		2h    	3h     	6h     	12h    	1d     	2d      	4d      	1w      	2w
+    3600, 		7200, 	10800, 	21600, 	43200, 	86400, 	172800, 	345600, 	604800, 	1209600,
+
+--  1mo      	2mo		3mo      	6mo       	1yr       	2y        	5y         	10y		20y		50y
+    2592000, 	5184000,	7776000, 	15552000, 	31536000, 	63072000, 	157680000, 	315360000, 	630720000,	1576800000
 })
+--  OBS: One month (1mo) is approximated by 30 days.
+
 
 openspace.bindKey("q", helper.renderable.toggle('SunMarker'))
 openspace.bindKey("e", helper.renderable.toggle('EarthMarker'))
