0f452b8f8b010b05959b21191674380001a271e2
kalbl <kallebladin@gmail.com>
2016-10-17 16:12:42 +0200

    Comment and document code.

--- modules/globebrowsing/chunk/chunkrenderer.h
+++ modules/globebrowsing/chunk/chunkrenderer.h
@@ -57,12 +57,35 @@ namespace openspace {
         ChunkRenderer(std::shared_ptr<Grid> grid,
             std::shared_ptr<TileProviderManager> tileProviderManager);
 
+        /**
+         Chooses to render a chunk either locally or globally depending on the chunklevel
+         of the <code>Chunk</code>.
+        */
         void renderChunk(const Chunk& chunk, const RenderData& data);
         void update();
 
     private:
-        
+        /**
+         Chunks can be rendered either globally or locally. Global rendering is performed
+         in the model space of the globe. With global rendering, the vertex positions
+         of a chunk are calculated in the vertex shader by transforming the geodetic
+         coordinates of the chunk to model space coordinates. We can only achieve floating
+         point precision by doing this which means that the camera too close to a global
+         tile will lead to jagging. We only render global chunks for lower chunk levels.
+        */
         void renderChunkGlobally(const Chunk& chunk, const RenderData& data);
+
+        /**
+         Local rendering of chunks are done using linear interpolation in camera space.
+         All four corner points of the chunk are calculated in double precision on the
+         CPU and transformed to camera space with double precision matrix transforms.
+         These positions can then be cast to floats and uploaded to the vertex shader.
+         The vertex shader rendering performs linear interpolation between the four
+         corner points to get the resulting chunk. This means that there will be an error
+         due to the curvature of the globe. The smaller the patch is (with higher chunk
+         levels) the better the approximation becomes. This is why we only render local
+         chunks for higher chunk levels.
+        */
         void renderChunkLocally(const Chunk& chunk, const RenderData& data);
 
         void setDepthTransformUniforms(
@@ -91,19 +114,17 @@ namespace openspace {
             std::shared_ptr<LayeredTextureShaderUniformIdHandler> programUniformHandler,
             const Chunk& chunk);
 
-        //////////////////////////////////////////////////////////////////////////////////
-        //                              Member variables                                //
-        //////////////////////////////////////////////////////////////////////////////////
-
+        // shared pointer to a grid which can be the same for all rendered chunks.
         std::shared_ptr<Grid> _grid;
         std::shared_ptr<TileProviderManager> _tileProviderManager;
 
+        // Two different shader programs. One for global and one for local rendering.
         std::shared_ptr<LayeredTextureShaderProvider> _globalRenderingShaderProvider;
         std::shared_ptr<LayeredTextureShaderProvider> _localRenderingShaderProvider;
 
+        // Layered texture uniforms are chached in the uniform ID handles.
         std::shared_ptr<LayeredTextureShaderUniformIdHandler> _globalProgramUniformHandler;
         std::shared_ptr<LayeredTextureShaderUniformIdHandler> _localProgramUniformHandler;
-
     };
 
 }  // namespace openspace
