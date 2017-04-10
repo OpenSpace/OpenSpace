98f26f8feb520790feebc44bc4f49e5243d4bd27
Michael Sjöström <micke_moj@hotmail.com>
2017-04-10 16:38:28 -0400

    Added new loadjob type that loads a ModelGeometry and used to load rover derived models on the fly

---
+++ modules/globebrowsing/tile/loadjob/surfacemodelloadjob.h
@@ -0,0 +1,56 @@
+/*****************************************************************************************
+*                                                                                       *
+* OpenSpace                                                                             *
+*                                                                                       *
+* Copyright (c) 2014-2017                                                               *
+*                                                                                       *
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this  *
+* software and associated documentation files (the "Software"), to deal in the Software *
+* without restriction, including without limitation the rights to use, copy, modify,    *
+* merge, publish, distribute, sublicense, and/or sell copies of the Software, and to    *
+* permit persons to whom the Software is furnished to do so, subject to the following   *
+* conditions:                                                                           *
+*                                                                                       *
+* The above copyright notice and this permission notice shall be included in all copies *
+* or substantial portions of the Software.                                              *
+*                                                                                       *
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,   *
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A         *
+* PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT    *
+* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF  *
+* CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE  *
+* OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                         *
+****************************************************************************************/
+
+#ifndef __OPENSPACE_MODULE_GLOBEBROWSING__SURFACEMODELLOADJOB__H_
+#define __OPENSPACE_MODULE_GLOBEBROWSING__SURFACEMODELLOADJOB__H_
+
+#include <modules/globebrowsing/tile/loadjob/loadjob2.h>
+#include <modules/globebrowsing/other/concurrentjobmanager.h>
+#include <ghoul/misc/dictionary.h>
+
+#include <modules/base/rendering/modelgeometry.h>
+
+namespace openspace {
+namespace globebrowsing {
+	
+struct SurfaceModelLoadJob : LoadJob2 {
+	SurfaceModelLoadJob(const ghoul::Dictionary& dictionary)
+		: _dictionary(dictionary)
+	{}
+	
+	virtual ~SurfaceModelLoadJob() = default;
+ 	
+	virtual void execute() override;
+
+	virtual std::shared_ptr<modelgeometry::ModelGeometry> product() const override;
+
+protected:
+	ghoul::Dictionary _dictionary;
+	std::shared_ptr<modelgeometry::ModelGeometry> _model;
+};
+
+} // globebrowsing
+} // openspace
+
+#endif // __OPENSPACE_MODULE_GLOBEBROWSING__SURFACEMODELLOADJOB__H_
