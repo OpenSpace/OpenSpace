18c767f408c93400489a527a3d6ec1d9eb6062ce
Michael Sjöström <micke_moj@hotmail.com>
2017-04-20 18:34:19 -0400

    Added an async provider for surface models

---
+++ modules/globebrowsing/models/asyncsurfacemodelprovider.cpp
@@ -0,0 +1,73 @@
+/*****************************************************************************************
+*                                                                                       *
+* OpenSpace                                                                             *
+*                                                                                       *
+* Copyright (c) 2014-2017                                                               *
+*                                                                                       *
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this  *
+* software and associated documentation files (the "Software"), to deal in the Software *
+* without restriction, including without limitation the rights to use, copy, modify,    *
+* merge, publish, distribute, sublicense, and/or sell copies of the Software, and to    *
+* permit persons to whom the Software is furnished to do so, subject to the following   *
+* conditions:                                                                           *
+*                                                                                       *
+* The above copyright notice and this permission notice shall be included in all copies *
+* or substantial portions of the Software.                                              *
+*                                                                                       *
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,   *
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A         *
+* PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT    *
+* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF  *
+* CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE  *
+* OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                         *
+****************************************************************************************/
+
+#include <modules/globebrowsing/models/asyncsurfacemodelprovider.h>
+#include <ghoul/logging/logmanager.h>
+#include <modules/globebrowsing/tile/loadjob/surfacemodelloadjob.h>
+#include <ghoul/filesystem/filesystem.h>
+
+
+namespace {
+	const std::string _loggerCat = "AsyncSurfaceModelProvider";
+}
+
+namespace openspace {
+namespace globebrowsing {
+
+AsyncSurfaceModelProvider::AsyncSurfaceModelProvider(std::shared_ptr<ThreadPool> pool)
+	: _concurrentJobManager(pool)
+{
+
+}
+
+bool AsyncSurfaceModelProvider::enqueueModelIO(const ghoul::Dictionary& dictionary, const std::shared_ptr<Model> model) {
+	// TODO: Check if allready enqued.
+	if (satisfiesEnqueueCriteria(model->fileName)) {
+		auto job = std::make_shared<SurfaceModelLoadJob>(dictionary, model);
+		_concurrentJobManager.enqueueJob(job);
+
+		_enqueuedTileRequests[model->fileName] = "faaan";
+
+		return true;
+	}
+	return false;
+}
+
+std::vector<std::shared_ptr<Model>> AsyncSurfaceModelProvider::getLoadedModels() {
+	std::vector<std::shared_ptr<Model>> loadedModels;
+
+	if (_concurrentJobManager.numFinishedJobs() > 0) {
+		loadedModels.push_back(_concurrentJobManager.popFinishedJob()->product());
+	}
+	return loadedModels;
+}
+
+bool AsyncSurfaceModelProvider::satisfiesEnqueueCriteria(const std::string fileName) const
+{
+	return _enqueuedTileRequests.find(fileName) == _enqueuedTileRequests.end();
+}
+
+
+} // namespace globebrowsing
+} // namespace openspace
