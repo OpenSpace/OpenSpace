a2ab4dfd30d97e2ad21dbf244420c392e7a647a3
Michael Novén <michael.f.noven@gmail.com>
2017-04-25 14:58:38 -0400

    Create a wrapper around openjpeg that handles decoding of tiles and converting a stream to a tiled .jp2 or .j2k

---
+++ modules/solarbrowsing/util/simplej2kcodec.h
@@ -0,0 +1,52 @@
+#ifndef SIMPLEJ2KCODEC_H
+#define SIMPLEJ2KCODEC_H
+
+#include "openjpeg.h"
+#include <string>
+#include <memory>
+
+struct ImageData {
+  int32_t* data;
+  uint32_t w;
+  uint32_t h;
+};
+
+class SimpleJ2kCodec {
+public:
+  SimpleJ2kCodec();
+  ~SimpleJ2kCodec();
+  // Decode tile from current loaded file
+  std::unique_ptr<ImageData> DecodeTile(const int& tileId);
+  // Decode whole image from current loaded file
+  std::unique_ptr<ImageData> Decode();
+  // Encodes current loaded file
+  void EncodeAsTiles(const char* outfile,
+                     const int32_t* data,
+                     const unsigned int imageWidth,
+                     const unsigned int imageHeight,
+                     const unsigned int tileWidth,
+                     const unsigned int tileHeight,
+                     const unsigned int numComps,
+                     const unsigned int compPrec);
+  void CreateInfileStream(const std::string& filename);
+  void SetResolutionFactor(const int resolution);
+
+private:
+  const int GetInfileFormat(const char* fname);
+  void Destroy();
+  void SetupDecoder();
+  void SetupEncoder();
+
+  bool _isFileLoaded;
+  bool _isDecoderSetup;
+  char _infileName[OPJ_PATH_LEN];
+
+  // Decoding will happen frequently - store the setup
+  opj_stream_t* _infileStream;
+  opj_codec_t* _decoder;
+  opj_dparameters_t _decoderParams;
+  opj_image_t* _image;
+  opj_codestream_info_v2_t* _codestreamInfo;
+};
+
+#endif // SIMPLEJ2KCODEC_H
