3766c9f7e3b3f4c80a083778c7faa72319c0aa1f
Emil Axelsson <emil.axelsson@liu.se>
2017-06-03 15:26:58 +0200

    Add missing shaders

---
+++ modules/volume/shaders/helper.glsl
@@ -0,0 +1,42 @@
+/*****************************************************************************************
+ *                                                                                       *
+ * OpenSpace                                                                             *
+ *                                                                                       *
+ * Copyright (c) 2014-2017                                                               *
+ *                                                                                       *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this  *
+ * software and associated documentation files (the "Software"), to deal in the Software *
+ * without restriction, including without limitation the rights to use, copy, modify,    *
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to    *
+ * permit persons to whom the Software is furnished to do so, subject to the following   *
+ * conditions:                                                                           *
+ *                                                                                       *
+ * The above copyright notice and this permission notice shall be included in all copies *
+ * or substantial portions of the Software.                                              *
+ *                                                                                       *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,   *
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A         *
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT    *
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF  *
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE  *
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                         *
+ ****************************************************************************************/
+
+#define KAMELEON_PI      3.14159265358979323846  /* pi */
+#define KAMELEON_SQRT1_3 0.57735026919           /* 1/sqrt(3) */
+
+vec3 kameleon_cartesianToSpherical(vec3 zeroToOneCoords) {
+    // Put cartesian in [-1..1] range first
+    const vec3 cartesian = vec3(-1.0,-1.0,-1.0) + zeroToOneCoords * 2.0f;
+
+    const float r = length(cartesian);
+    float theta, phi;
+
+    if (r == 0.0) {
+        theta = phi = 0.0;
+    } else {
+        theta = acos(cartesian.z/r) / KAMELEON_PI;
+        phi = (KAMELEON_PI + atan(cartesian.y, cartesian.x)) / (2.0*KAMELEON_PI );
+    }
+    return vec3(r * KAMELEON_SQRT1_3, theta, phi);
+}
