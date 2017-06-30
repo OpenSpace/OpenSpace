26f4d69b2360bba4221a40ce99fbad240607ed76
Klas Eskilson <klas.eskilson@gmail.com>
2017-06-30 14:47:38 -0400

    [WebGui] Add SmallLabel component

---
+++ modules/webgui/web/src/components/common/SmallLabel/SmallLabel.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './SmallLabel.scss';
+
+const SmallLabel = (props) => {
+  const { children } = props;
+  return (
+    <span {...props} className={styles.SmallLabel}>
+      { children }
+    </span>
+  );
+};
+
+SmallLabel.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.node),
+  ]),
+};
+
+SmallLabel.defaultProps = {
+  children: [],
+};
+
+export default SmallLabel;
