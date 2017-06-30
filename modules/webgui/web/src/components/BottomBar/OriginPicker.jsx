776f626019785bc0e8a92b483f3856143a9d4f14
Klas Eskilson <klas.eskilson@gmail.com>
2017-06-30 12:14:37 -0400

    [WebGui] Add WIP origin picker

---
+++ modules/webgui/web/src/components/BottomBar/OriginPicker.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import styles from './OriginPicker.scss';
+
+class OriginPicker extends Component {
+  render() {
+    return (
+      <div className={styles.OriginPicker}>
+        pick!
+      </div>
+    );
+  }
+}
+
+export default OriginPicker;
