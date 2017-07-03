5c68674ffaabdfae5fe21afd3cb134c3ada55ff3
Klas Eskilson <klas.eskilson@gmail.com>
2017-07-03 14:16:07 -0400

    [WebGui] Add SmallLabel story
    Also fix `styling` references in Icon story.

--- modules/webgui/web/src/components/common/Icon/Icon.story.jsx
+++ modules/webgui/web/src/components/common/Icon/Icon.story.jsx
@@ -1,3 +1,4 @@
+/* eslint import/no-extraneous-dependencies: 0 */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Icon from './Icon';
@@ -6,8 +7,8 @@ import Icon from './Icon';
 
 storiesOf('Icon', module)
   .add('no options', () => (<Icon icon="language" />))
-  .add('small', () => (<Icon icon="language" styling={['small']} />))
-  .add('normal', () => (<Icon icon="language" styling={['normal']} />))
-  .add('medium', () => (<Icon icon="language" styling={['medium']} />))
-  .add('large', () => (<Icon icon="language" styling={['large']} />))
-  .add('extralarge', () => (<Icon icon="language" styling={['extralarge']} />));
+  .add('styling=small', () => (<Icon icon="language" styling="small" />))
+  .add('styling=normal', () => (<Icon icon="language" styling="normal" />))
+  .add('styling=medium', () => (<Icon icon="language" styling="medium" />))
+  .add('styling=large', () => (<Icon icon="language" styling="large" />))
+  .add('styling=extralarge', () => (<Icon icon="language" styling="extralarge" />));
