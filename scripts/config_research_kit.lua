d905f7be285c9a44f15e46f1f0e7ecc737aab221
Matthew Territo <territo-sci@users.noreply.github.com>
2017-07-13 13:29:33 -0600

    Set shen scene in config, turn on timer

--- scripts/config_research_kit.lua
+++ scripts/config_research_kit.lua
@@ -1,5 +1,5 @@
 --[[ Configuration scripts for the Research Kit ]]--
 
 -- Set a timeout for the application
-openspace.setPropertyValue("Global Properties.ResearchKit.Timer._timeout", 20000)
+openspace.setPropertyValue("Global Properties.ResearchKit.Timer._timeout", 4000)
 openspace.setPropertyValue("Global Properties.ResearchKit.Timer._timeoutCycles", 0)
