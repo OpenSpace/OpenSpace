c084db272448b1afe7017731dab17f20118155c9
Klas Eskilson <klas.eskilson@gmail.com>
2017-07-17 14:19:39 -0400

    [WebGui] Add Popover close button and header

--- modules/webgui/web/src/components/common/Popover/Popover.jsx
+++ modules/webgui/web/src/components/common/Popover/Popover.jsx
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import Icon from '../Icon/Icon';
 import styles from './Popover.scss';
+
+const findStyles = arr => arr.split(' ')
+  .map(style => styles[style])
+  .join(' ');
 
 class Popover extends Component {
   get arrowStyle() {
-    return this.props.arrow
-      .split(' ')
-      .map(style => styles[style])
-      .join(' ');
+    return findStyles(this.props.arrow);
+  }
+
+  get styles() {
+    return findStyles(this.props.styles);
   }
 
   render() {
     return (
-      <section className={`${styles.popover} ${this.arrowStyle}`}>
+      <section className={`${styles.popover} ${this.arrowStyle} ${this.styles}`}>
+        { this.props.title &&
+          (<header>
+            <div className={styles.title}>
+              { this.props.title }
+            </div>
+
+            { this.props.closeCallback &&
+              (<button onClick={this.props.closeCallback} className={styles.close}>
+                <Icon icon="close" styling="small" />
+              </button>) }
+          </header>)}
         { this.props.children }
       </section>
     );
@@ -22,10 +39,19 @@ class Popover extends Component {
 
 Popover.propTypes = {
   arrow: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  closeCallback: PropTypes.func,
+  styles: PropTypes.string,
+  title: PropTypes.string,
 };
 
 Popover.defaultProps = {
   arrow: 'arrow bottom center',
+  closeCallback: null,
+  styles: '',
+  title: null,
 };
+
+Popover.styles = styles;
 
 export default Popover;
--- modules/webgui/web/src/components/common/Popover/Popover.story.jsx
+++ modules/webgui/web/src/components/common/Popover/Popover.story.jsx
@@ -1,6 +1,7 @@
 /* eslint import/no-extraneous-dependencies: 0 */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import Popover from './Popover';
 
 /* globals module */
@@ -9,4 +10,6 @@ storiesOf('Popover', module)
   .add('default (arrow bottom center)', () => (<Popover>Hello!</Popover>))
   .add('arrow bottom left', () => (<Popover arrow="arrow bottom leftside">Hello!</Popover>))
   .add('arrow bottom right', () => (<Popover arrow="arrow bottom rightside">Hello!</Popover>))
+  .add('small', () => (<Popover arrow="arrow bottom rightside" styles="small">Hello!</Popover>))
+  .add('with title', () => (<Popover title="title" closeCallback={action('close')}>Hello!</Popover>))
   .add('no arrow', () => (<Popover arrow="">Hello!</Popover>));
