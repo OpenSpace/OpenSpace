ff566877375ae37d3e99347ace39222ba859bfb9
Matthew Territo <territo-sci@users.noreply.github.com>
2017-07-17 13:44:48 -0600

    Add RenderEngine performanceMeasurements toggle on to scene scripts

--- scripts/config_mrv_logging.lua
+++ scripts/config_mrv_logging.lua
@@ -1,2 +1,3 @@
 openspace.setPropertyValue("Global Properties.OnScreenGUI.Main.PerformanceComponent.enabled", true)
 openspace.setPropertyValue("Global Properties.OnScreenGUI.Main.PerformanceComponent.outputLogs", true)
+openspace.setPropertyValue("RenderEngine.performanceMeasurements", true)
