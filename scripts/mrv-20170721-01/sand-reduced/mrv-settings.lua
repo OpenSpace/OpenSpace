c6c18a5f01b2c1fa901ebc2092332e99d4a56c52
Matthew Territo <territo-sci@users.noreply.github.com>
2017-07-21 14:21:25 -0600

    Set of scripts with spatial tolerance 1.0 and memory budget 128

---
+++ scripts/mrv-20170721-01/sand-reduced/mrv-settings.lua
@@ -0,0 +1,8 @@
+dofile(openspace.absPath('${SCRIPTS}/config_mrv.lua'))
+openspace.setPropertyValue(MRV_CONFIG_ENLIL_RENDERABLE .. ".spatialTolerance", 1.0)
+openspace.setPropertyValue(MRV_CONFIG_ENLIL_RENDERABLE .. ".temporalTolerance", 0.0)
+
+openspace.setPropertyValue(MRV_CONFIG_ENLIL_RENDERABLE .. ".scalingExponent", 12)
+openspace.setPropertyValue(MRV_CONFIG_ENLIL_RENDERABLE .. ".scaling", {0.7, 0.7, 0.7})
+
+
---
+++ scripts/mrv-20170721-01/shen/mrv-settings.lua
@@ -0,0 +1,3 @@
+dofile(openspace.absPath('${SCRIPTS}/config_mrv.lua'))
+openspace.setPropertyValue(MRV_CONFIG_ENLIL_RENDERABLE .. ".spatialTolerance", 1.0)
+openspace.setPropertyValue(MRV_CONFIG_ENLIL_RENDERABLE .. ".temporalTolerance", 0.0)
