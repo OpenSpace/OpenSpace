0134a77457b4059b3d452a03dc9b33027cdc665a
Klas Eskilson <klas.eskilson@gmail.com>
2017-08-01 16:39:12 -0400

    [WebGui] Ensure Error has a default class when not explicitly set

--- modules/webgui/web/src/components/common/Error/Error.jsx
+++ modules/webgui/web/src/components/common/Error/Error.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import styles from './Error.scss';
 
-const Error = ({ children, className = styles.error }) => (
+const Error = ({ children, className }) => (
   <div className={`${className} ${styles.ErrorBox}`}>
     { children }
   </div>
@@ -12,6 +12,10 @@ const Error = ({ children, className = styles.error }) => (
 Error.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
+};
+
+Error.defaultProps = {
+  className: styles.error,
 };
 
 export default Error;
