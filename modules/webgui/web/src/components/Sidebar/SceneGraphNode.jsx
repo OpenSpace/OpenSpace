44aba76b3c4a5e4da5f712637b8026b88cfc8cc0
Klas Eskilson <klas.eskilson@gmail.com>
2017-08-11 19:00:36 -0400

    [WebGui] Add Focus button to SceneGraphNodes in sidebar

--- modules/webgui/web/src/components/Sidebar/SceneGraphNode.jsx
+++ modules/webgui/web/src/components/Sidebar/SceneGraphNode.jsx
@@ -2,19 +2,32 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import PropertyCollection from './PropertyCollection';
 import ToggleContent from '../common/ToggleContent/ToggleContent';
-
+import Button from '../common/Input/Button';
+import Icon from '../common/Icon/Icon';
+import DataManager from '../../api/DataManager';
+import { OriginKey } from '../../api/keys';
 import styles from './SceneGraphNode.scss';
 
 class SceneGraphNode extends Component {
   constructor(props) {
     super(props);
+
+    this.focusOnThis = this.focusOnThis.bind(this);
+  }
+
+  focusOnThis() {
+    const { name } = this.props;
+    DataManager.setValue(OriginKey, name);
   }
 
   render() {
-    const { name, properties, renderable } = this.props;
+    const { name, renderable } = this.props;
 
     return (
       <ToggleContent title={name}>
+        <Button onClick={this.focusOnThis}>
+          <Icon icon="gps_fixed" /> Focus
+        </Button>
         { renderable && (<PropertyCollection name="Renderable" properties={renderable.properties} />) }
       </ToggleContent>
     );
@@ -23,7 +36,6 @@ class SceneGraphNode extends Component {
 
 SceneGraphNode.propTypes = {
   name: PropTypes.string.isRequired,
-  properties: PropTypes.arrayOf(PropTypes.object),
   // eslint-disable-next-line react/forbid-prop-types
   renderable: PropTypes.object,
 };
