149d51fc29fca26b2deaa92cd5d219ad7ac5ffd7
Klas Eskilson <klas.eskilson@gmail.com>
2017-08-11 18:54:23 -0400

    [WebGui] Add api/keys.js to store some common used keys and strings

---
+++ modules/webgui/web/src/api/keys.js
@@ -0,0 +1,10 @@
+// eslint-disable import/prefer-default-export
+
+// key for setting/getting the Origin (focus) node
+export const OriginKey = 'NavigationHandler.Origin';
+// key to get all scene graph nodes
+export const SceneGraphKey = '__allNodes';
+// script to toggle pause
+export const TogglePauseScript = 'openspace.time.togglePause()';
+// key for getting current time subscription
+export const TimeKey = 'special:currentTime';
