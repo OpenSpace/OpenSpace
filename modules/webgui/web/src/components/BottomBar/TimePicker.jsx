2eeb0363e11c425785e7e98d7d3df1491af0853c
Klas Eskilson <klas.eskilson@gmail.com>
2017-08-11 14:42:26 -0400

    [WebGui] Add support for setting current time

--- modules/webgui/web/src/components/BottomBar/TimePicker.jsx
+++ modules/webgui/web/src/components/BottomBar/TimePicker.jsx
@@ -6,8 +6,18 @@ import SmallLabel from '../common/SmallLabel/SmallLabel';
 import Button from '../common/Input/Button';
 import Calendar from '../common/Calendar/Calendar';
 import Picker from './Picker';
+import Time from '../common/Input/Time';
 
 const TIME_KEY = 'special:currentTime';
+
+/**
+ * Make sure the date string contains a time zone
+ * @param date
+ * @param zone - the time zone in ISO 8601 format
+ * @constructor
+ */
+const DateStringWithTimeZone = (date, zone = 'Z') =>
+  (!date.includes('Z') ? `${date}${zone}` : date);
 
 class TimePicker extends Component {
   constructor(props) {
@@ -21,6 +31,7 @@ class TimePicker extends Component {
 
     this.subscriptionCallback = this.subscriptionCallback.bind(this);
     this.togglePopover = this.togglePopover.bind(this);
+    this.setDate = this.setDate.bind(this);
   }
 
   componentDidMount() {
@@ -41,24 +52,48 @@ class TimePicker extends Component {
    * @param message [object] - message object sent from Subscription
    */
   subscriptionCallback(message) {
-    const newState = {
-      time: new Date(message.time),
-    };
-    if (!this.state.hasTime) {
-      newState.hasTime = true;
-    }
-    this.setState(previous => Object.assign({}, previous, newState));
+    const time = new Date(DateStringWithTimeZone(message.time));
+    this.setState({ time, hasTime: true });
+  }
+
+  setDate(time) {
+    this.setState({ time });
+    // Spice, that is handling the time parsing in OpenSpace does not suppport
+    // ISO 8601-style time zones (the Z). It does, however, always assume that UTC
+    // is given.
+    const fixedTimeString = time.toJSON().replace('Z', '');
+    DataManager.setValue('__time', fixedTimeString);
   }
 
   get time() {
     return this.state.time.toUTCString();
   }
 
+  get popover() {
+    const { time } = this.state;
+    return (
+      <Popover className={Picker.Popover} title="Select date" closeCallback={this.togglePopover}>
+        <Calendar selected={time} activeMonth={time} onChange={this.setDate} />
+        <hr className={Popover.styles.delimiter} />
+        <div className={Popover.styles.title}>Select time</div>
+        <div className={Popover.styles.content}>
+          <Time time={time} onChange={this.setDate} />
+        </div>
+        <hr className={Popover.styles.delimiter} />
+        <p>
+          <Button block disabled onClick={this.togglePause}>
+            Pause simulation
+          </Button>
+        </p>
+      </Popover>
+    );
+  }
+
   render() {
-    const { showPopover, time } = this.state;
+    const { showPopover } = this.state;
     return (
       <div className={Picker.Wrapper}>
-        <Picker onClick={this.togglePopover} className={(showPopover && Picker.Active)}>
+        <Picker onClick={this.togglePopover} className={(showPopover ? Picker.Active : '')}>
           <div className={Picker.Title}>
             <span className={Picker.Name}>
               <LoadingString loading={!this.state.hasTime}>
@@ -68,17 +103,7 @@ class TimePicker extends Component {
             <SmallLabel>Date</SmallLabel>
           </div>
         </Picker>
-        { showPopover && (
-          <Popover className={Picker.Popover} title="Select time" closeCallback={this.togglePopover}>
-            <Calendar selected={time} activeMonth={time} />
-            <hr className={Popover.styles.delimiter} />
-            <p>
-              <Button block disabled onClick={this.togglePause}>
-                Pause simulation
-              </Button>
-            </p>
-          </Popover>
-        )}
+        { showPopover && this.popover }
       </div>
     );
   }
