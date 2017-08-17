57c560d7f65bddf224f81a568add7ec53ca8da1a
Klas Eskilson <klas.eskilson@gmail.com>
2017-08-17 19:17:01 -0400

    [WebGui] Add incremental step size selection

--- modules/webgui/web/src/components/BottomBar/SimulationIncrement.jsx
+++ modules/webgui/web/src/components/BottomBar/SimulationIncrement.jsx
@@ -4,6 +4,8 @@ import DataManager, { TopicTypes } from '../../api/DataManager';
 import { DeltaTime, ValuePlaceholder, SetDeltaTimeScript } from '../../api/keys';
 import NumericInput from '../common/Input/NumericInput';
 import Row from '../common/Row/Row';
+import Select from '../common/Input/Select';
+import { round10 } from '../../utils/rounding';
 
 const UpdateDelayMs = 1000;
 // Throttle the delta time updating, so that we don't accidentally flood
@@ -13,13 +15,54 @@ const UpdateDeltaTime = throttle((value) => {
   DataManager.runScript(script);
 }, UpdateDelayMs);
 
+const Steps = {
+  seconds: 'Seconds',
+  minutes: 'Minutes',
+  hours: 'Hours',
+  days: 'Days',
+  months: 'Months',
+  years: 'Years',
+};
+const StepSizes = {
+  [Steps.seconds]: 1,
+  [Steps.minutes]: 60,
+  [Steps.hours]: 3600,
+  [Steps.days]: 86400,
+  [Steps.months]: 2678400,
+  [Steps.years]: 31536000,
+};
+const StepPrecisions = {
+  [Steps.seconds]: 0,
+  [Steps.minutes]: -3,
+  [Steps.hours]: -4,
+  [Steps.days]: -7,
+  [Steps.months]: -10,
+  [Steps.years]: -14,
+};
+const Limits = {
+  [Steps.seconds]: { min: -1000000, max: 1000000, step: 1 },
+  [Steps.minutes]: { min: -100000, max: 100000, step: 0.001 },
+  [Steps.hours]: { min: -1000, max: 1000, step: 0.0001 },
+  [Steps.days]: { min: -10, max: 10, step: 0.000001 },
+  [Steps.months]: { min: -10, max: 10, step: 0.00000001 },
+  [Steps.years]: { min: -1, max: 1, step: 0.0000000001 },
+};
+Object.freeze(Steps);
+Object.freeze(StepSizes);
+Object.freeze(StepPrecisions);
+Object.freeze(Limits);
+
 class SimulationIncrement extends Component {
   constructor(props) {
     super(props);
-    this.state = { deltaTime: 1 };
+    this.state = {
+      deltaTime: 1,
+      stepSize: Steps.seconds,
+    };
 
     this.deltaTimeUpdated = this.deltaTimeUpdated.bind(this);
     this.setDeltaTime = this.setDeltaTime.bind(this);
+    this.setStepSize = this.setStepSize.bind(this);
   }
 
   componentDidMount() {
@@ -30,11 +73,30 @@ class SimulationIncrement extends Component {
     DataManager.unsubscribe(DeltaTime, this.deltaTimeUpdated);
   }
 
+  get stepSize() {
+    const { stepSize } = this.state;
+    return StepSizes[stepSize];
+  }
+
+  get limits() {
+    const { stepSize } = this.state;
+    return Limits[stepSize];
+  }
+
   setDeltaTime(event) {
     const { value } = event.currentTarget;
+    const deltaTime = parseFloat(value) * this.stepSize;
+
     // optimistic ui change
-    this.setState({ deltaTime: value });
-    UpdateDeltaTime(value);
+    this.setState({ deltaTime });
+    UpdateDeltaTime(deltaTime);
+  }
+
+  setStepSize(event) {
+    const { value } = event.currentTarget;
+    if (!Object.values(Steps).includes(value)) return;
+
+    this.setState({ stepSize: value });
   }
 
   deltaTimeUpdated({ deltaTime }) {
@@ -42,14 +104,25 @@ class SimulationIncrement extends Component {
   }
 
   render() {
+    const { deltaTime, stepSize } = this.state;
+    const adjustedDelta = round10(deltaTime / this.stepSize, StepPrecisions[stepSize]);
+
     return (
       <Row>
-        {/* TODO: Add debouncing support to inputs */}
+        <Select label="Step size" onChange={this.setStepSize} value={stepSize}>
+          <option value={Steps.seconds}>Seconds</option>
+          <option value={Steps.minutes}>Minutes</option>
+          <option value={Steps.hours}>Hours</option>
+          <option value={Steps.days}>Days</option>
+          <option value={Steps.months}>Months</option>
+          <option value={Steps.years}>Years</option>
+        </Select>
+
         <NumericInput
-          value={this.state.deltaTime}
-          placeholder="Seconds per step"
-          min={-100000}
-          max={100000}
+          value={adjustedDelta}
+          placeholder={`${stepSize} per step`}
+          step={0.0000001}
+          {...this.limits}
           onChange={this.setDeltaTime}
         />
       </Row>
