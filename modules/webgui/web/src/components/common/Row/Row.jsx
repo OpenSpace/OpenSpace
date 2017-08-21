c005abf593b38d5f9b250d7189d0bd68ab030a66
Klas Eskilson <klas.eskilson@gmail.com>
2017-08-21 17:07:55 -0400

    [WebGui] Inherit className in Row properly

--- modules/webgui/web/src/components/common/Row/Row.jsx
+++ modules/webgui/web/src/components/common/Row/Row.jsx
@@ -3,13 +3,18 @@ import PropTypes from 'prop-types';
 import styles from './Row.scss';
 
 const Row = props => (
-  <div {...props} className={styles.row}>
+  <div {...props} className={`${styles.row} ${props.className}`}>
     { props.children }
   </div>
 );
 
 Row.propTypes = {
   children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+};
+
+Row.defaultProps = {
+  className: '',
 };
 
 export default Row;
