be325615dae6cb471b15e1a2c9cba386d1385afa
Klas Eskilson <klas.eskilson@gmail.com>
2017-08-21 15:10:36 -0400

    [WebGui] Add react-router and implementation to App

--- modules/webgui/web/src/App.jsx
+++ modules/webgui/web/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import OnScreenGui from './views/OnScreenGui';
+import NotFound from './views/NotFound';
 
 // TODO: add routing
 const App = () => (
-  <OnScreenGui />
+  <Router>
+    <Switch>
+      <Route path="/onscreen" component={OnScreenGui} />
+      {/* Here, more GUI variations can be added. */}
+      {/* <Route path="/tablet" component={TabletGui} /> */}
+      <Route component={NotFound} />
+    </Switch>
+  </Router>
 );
 
 export default App;
