521cb0f5ae32de9183c3565efcada1531da7e341
Emil Axelsson <emil.axelsson@liu.se>
2018-01-11 14:40:16 +0100

    Use framebuffer size to determine gui size

--- modules/imgui/imguimodule.cpp
+++ modules/imgui/imguimodule.cpp
@@ -169,8 +169,10 @@ ImGUIModule::ImGUIModule() : OpenSpaceModule(Name) {
             WindowWrapper& wrapper = OsEng.windowWrapper();
             bool showGui = wrapper.hasGuiWindow() ? wrapper.isGuiWindow() : true;
             if (wrapper.isMaster() && showGui) {
+                const glm::ivec2 windowSize = wrapper.currentWindowSize();
+                const glm::ivec2 resolution = wrapper.currentWindowResolution();
+
                 glm::vec2 mousePosition = wrapper.mousePosition();
-                glm::ivec2 windowSize = wrapper.currentWindowSize();
                 uint32_t mouseButtons = wrapper.mouseButtons(2);
 
                 double dt = std::max(wrapper.averageDeltaTime(), 0.0);
@@ -183,7 +185,7 @@ ImGUIModule::ImGUIModule() : OpenSpaceModule(Name) {
                 gui.startFrame(
                     static_cast<float>(dt),
                     glm::vec2(windowSize),
-                    wrapper.dpiScaling(),
+                    resolution / windowSize,
                     mousePosition,
                     mouseButtons
                 );
