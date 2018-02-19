9ad1d7e3ed8accc4a7eefead5ff15afb5db6a8d4
Alexander Bock <alexander.bock@me.com>
2018-02-19 16:29:38 -0500

    Reduce default sensitivity of Orbital Navigator
    Rename internal macros to reduce compiler warnings

--- include/openspace/performance/performancemeasurement.h
+++ include/openspace/performance/performancemeasurement.h
@@ -48,12 +48,12 @@ private:
     std::chrono::high_resolution_clock::time_point _startTime;
 };
 
-#define __MERGE(a,b)  a##b
-#define __LABEL(a) __MERGE(unique_name_, a)
+#define __MERGE_PerfMeasure(a,b)  a##b
+#define __LABEL_PerfMeasure(a) __MERGE_PerfMeasure(unique_name_, a)
 
 /// Declare a new variable for measuring the performance of the current block
 #define PerfMeasure(name) \
-    auto __LABEL(__LINE__) = \
+    auto __LABEL_PerfMeasure(__LINE__) = \
         openspace::performance::PerformanceMeasurement(\
             (name), \
             OsEng.renderEngine().performanceManager() \
