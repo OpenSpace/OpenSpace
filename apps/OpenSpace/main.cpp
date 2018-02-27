3ce9f523a697e293324b99132971f546ef2a8313
Alexander Bock <alexander.bock@me.com>
2018-02-26 23:46:03 -0500

    Add new dedicated directory for screenshots
     * Add ability to have current date in file name path
     * Remove SGCT_ prefix for screenshots (closes #449)

--- apps/OpenSpace/main.cpp
+++ apps/OpenSpace/main.cpp
@@ -22,6 +22,7 @@
  * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                         *
  ****************************************************************************************/
 
+#include <openspace/engine/configurationmanager.h>
 #include <openspace/engine/openspaceengine.h>
 #include <openspace/engine/wrapper/sgctwindowwrapper.h>
 #include <openspace/util/keys.h>
@@ -31,6 +32,8 @@
 #include <ghoul/misc/boolean.h>
 
 #include <sgct.h>
+#include <chrono>
+#include <ctime>
 
 #ifdef WIN32
 
@@ -306,6 +309,41 @@ void mainInitFunc() {
 
 #endif // OPENSPACE_HAS_SPOUT
     }
+    
+    std::string k = openspace::ConfigurationManager::KeyScreenshotUseDate;
+    std::string screenshotPath = "${SCREENSHOTS}";
+    std::string screenshotNames = "OpenSpace";
+    if (OsEng.configurationManager().hasKey(k)) {
+        std::time_t now = std::time(nullptr);
+        std::tm* nowTime = std::localtime(&now);
+        char mbstr[100];
+        strftime(mbstr, sizeof(mbstr), "%Y-%m-%d-%H-%M", nowTime);
+        screenshotPath += "/" + std::string(mbstr);
+
+        FileSys.createDirectory(absPath(screenshotPath));
+    }
+
+    for (size_t i = 0; i < nWindows; ++i) {
+        sgct_core::ScreenCapture* cpt0 =
+            SgctEngine->getWindowPtr(i)->getScreenCapturePointer(0);
+        sgct_core::ScreenCapture* cpt1 =
+            SgctEngine->getWindowPtr(i)->getScreenCapturePointer(1);
+        
+        if (cpt0) {
+            cpt0->setPathAndFileName(
+                absPath(screenshotPath),
+                screenshotNames
+            );
+        }
+
+        if (cpt1) {
+            cpt1->setPathAndFileName(
+                screenshotPath,
+                screenshotNames
+            );
+        }
+    }
+
     LTRACE("main::mainInitFunc(end)");
 }
 
