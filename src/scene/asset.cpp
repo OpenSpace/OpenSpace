fb81216b5a20e1d9443f7147c18cdb53c1e4d4a7
Emil Axelsson <emil.axelsson@liu.se>
2018-03-07 10:20:45 +0100

    Print error message when a resource synchronization is rejected

--- src/scene/asset.cpp
+++ src/scene/asset.cpp
@@ -175,8 +175,8 @@ void Asset::addSynchronization(std::shared_ptr<ResourceSynchronization> synchron
     SynchronizationWatcher::WatchHandle watch =
         _synchronizationWatcher->watchSynchronization(
             synchronization,
-            [this](ResourceSynchronization::State state) {
-                syncStateChanged(state);
+            [this, synchronization](ResourceSynchronization::State state) {
+                syncStateChanged(synchronization, state);
             }
         );
     _syncWatches.push_back(watch);
@@ -189,12 +189,18 @@ void Asset::clearSynchronizations() {
     _syncWatches.clear();
 }
 
-void Asset::syncStateChanged(ResourceSynchronization::State state) {
+void Asset::syncStateChanged(std::shared_ptr<ResourceSynchronization> sync,
+                             ResourceSynchronization::State state)
+{
+
     if (state == ResourceSynchronization::State::Resolved) {
         if (!isSynchronized() && isSyncResolveReady()) {
             setState(State::SyncResolved);
         }
     } else if (state == ResourceSynchronization::State::Rejected) {
+        LERROR("Failed to synchronize resource \"" <<
+            sync->name() << "\" in asset \"" << id() << "\"");
+
         setState(State::SyncRejected);
     }
 }
