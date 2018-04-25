f9a56a85c28924f4be28690d6128d5ee04fe3fb5
Matthias Berg <mberg@gsfc674-ccmcviz.ndc.nasa.gov>
2018-04-25 15:33:31 -0400

    Add mas-transferfunction file

---
+++ data/mas-data/raw/masTransferfunction.tf
@@ -0,0 +1,9 @@
+width 5
+lower 0.0
+upper 1.0
+mappingkey 0.15 250 0 0     0
+mappingkey 0.20 250 0 0     20
+mappingkey 0.35 250 20 0    20
+mappingkey 0.40 250 0 0     0
+mappingkey 0.60 250 0 0     0
+mappingkey 0.80 0 0 200     255
