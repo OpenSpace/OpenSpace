d2b00ea76b2378a07c9352981673d00f20929e61
jon-grangien <jonathan.grangien@gmail.com>
2018-05-09 11:00:00 -0400

    Try with different volume rendering values for the data

--- data/mas/mas_transfer_function.tf
+++ data/mas/mas_transfer_function.tf
@@ -1,6 +1,12 @@
 width 5
 lower 0.0
 upper 1.0
+mappingkey 0.000001 250 0 0     0
+mappingkey 0.000015 250 0 0     20
+mappingkey 0.00025 250 20 0    20
+mappingkey 0.004 250 0 0     0
+mappingkey 0.06 250 0 0     0
+
 mappingkey 0.15 250 0 0     0
 mappingkey 0.20 250 0 0     20
 mappingkey 0.35 250 20 0    20
