46907011a3b6273b7c5ad93cc48d4313be304102
sofiekhullar <sofie_k@live.se>
2018-05-24 09:41:30 -0600

    [WebGui] Added infobutton and storyinfo in stories

--- modules/webgui/web/src/components/TouchBar/UtilitiesMenu/UtilitiesMenu.jsx
+++ modules/webgui/web/src/components/TouchBar/UtilitiesMenu/UtilitiesMenu.jsx
@@ -4,12 +4,14 @@ import HomeButtonContainer from './HomeButtonContainer';
 import Controllers from './Controllers';
 
 import styles from './UtilitiesMenu.scss';
+import InfoButtonController from './InfoButtonContainer';
 
 
 const UtilitiesMenu = () => (
   <div className={styles.UtilitiesMenu}>
     <HomeButtonContainer />
     <HelpButton />
+    <InfoButtonController />
     <Controllers />
   </div>
 );
