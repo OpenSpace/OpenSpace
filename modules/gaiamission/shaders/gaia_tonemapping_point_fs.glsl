8aad6ca8d111221e4b183eb6f327eb6fab8ddf09
Adam Alsegård <adam.alsegard@gmail.com>
2018-05-29 15:00:31 +0200

    Further improvements of elliptic rendering for domes.

--- modules/gaiamission/shaders/gaia_tonemapping_point_fs.glsl
+++ modules/gaiamission/shaders/gaia_tonemapping_point_fs.glsl
@@ -35,37 +35,52 @@ uniform float sigma;
 const float M_PI = 3.141592653589793238462;
 const float DEFAULT_DEPTH = 3.08567758e19; // 1000 Pc
 
-float atan2(in float y, in float x)
-{
-    bool s = (abs(x) > abs(y));
-    return mix(M_PI / 2.0 - atan(x,y), atan(y,x), s);
-}
-
 Fragment getFragment() {
 
     vec4 color = vec4(0.0);
     
     // GL_POINTS
-    // Scale filter to compensate for curved screens.
-    vec2 screenPos = (uv - 0.5) * 2.0; // [-1, 1]
-    vec2 fov = vec2(projection[0][0], projection[1][1]);
+    // Scale filter components to compensate for a skewed frustum!
+    float near = float(projection[3][2] / (projection[2][2] - 1.0));
+    float left = float(near * (projection[2][0] - 1.0) / projection[0][0]);
+    float right = float(near * (projection[2][0] + 1.0) / projection[0][0]);
+    float top = float(near * (projection[2][1] + 1.0) / projection[1][1]);
+    float bottom = float(near * (projection[2][1] - 1.0) / projection[1][1]);
+    
+    // Find screenPos in skewed frustum. uv is [0, 1]
+    //vec2 screenPos = (uv - 0.5) * 2.0; // [-1, 1]
+    vec2 screenPos = uv * vec2(right - left, top - bottom) + vec2(left, bottom); 
+    vec2 screenOrigo = vec2(-left, -bottom) / vec2(right - left, top - bottom);
     vec2 scaleFactor = vec2(
-        1.0 / (1.0 + pow(screenPos.x / fov.x, 2.0)), 
-        1.0 / (1.0 + pow(screenPos.y / fov.y, 2.0))
+        pow(screenPos.x / near, 2.0),  
+        pow(screenPos.y / near, 2.0)  
     );
+    scaleFactor *= 0.5;
+
+    // Use the following to find the origo in a skewed frustum.
+    //Fragment origoFrag;
+    //if (abs(screenOrigo.x - uv.x) > 0.0005 && abs(screenOrigo.y - uv.y) > 0.0005) {
+    //    origoFrag.color = vec4(0.0);
+    //} else {
+    //    origoFrag.color = vec4(1.0);
+    //}
+    //return origoFrag;
+
     // Uncomment to compare to original filterSize.
-    scaleFactor = vec2(1.0);
-    dvec2 newFilterSize = filterSize / scaleFactor;
+    //scaleFactor = vec2(1.0);
 
     // Make use of this to switch betweeen circle and ellipse.
     bool useCircleDist = false;
 
+    // Scale filter.
+    int newFilterSize = int(filterSize * (1 + length(scaleFactor)));
+
     // Get a [filterSize x filterSize] filter around our pixel. UV is [0, 1]
     vec3 intensity = vec3(0.0);
     vec2 pixelSize = 1.0 / screenSize;
-    vec2 halfFilterSize = vec2(newFilterSize - 1.0) / 2.0;
-    for (float y = -halfFilterSize.y; y <= halfFilterSize.y; y += 1.0) {
-        for (float x = -halfFilterSize.x; x <= halfFilterSize.x; x += 1.0) {
+    int halfFilterSize = newFilterSize - 1 / 2;
+    for (int y = -halfFilterSize; y <= halfFilterSize; y += 1) {
+        for (int x = -halfFilterSize; x <= halfFilterSize; x += 1) {
             vec2 sPoint = uv + (pixelSize * vec2(x, y));
             
             // Don't sample outside of the FBO texture.
@@ -74,27 +89,27 @@ Fragment getFragment() {
 
                 // Use normal distribution function for halo/bloom effect. 
                 if (useCircleDist) {
-                    float circleDist = sqrt(pow(x * scaleFactor.x, 2.0) + pow(y * scaleFactor.y, 2.0));
+                    float circleDist = sqrt(pow(x / (1 + length(scaleFactor)), 2.0)
+                        + pow(y / (1 + length(scaleFactor)), 2.0));
                     intensity += sIntensity.rgb * (1.0 / (sigma * sqrt(2.0 * M_PI))) * 
                         exp(-(pow(circleDist, 2.0) / (2.0 * pow(sigma, 2.0)))) / filterSize;
                     }
                 else {
                     // Elliptic gaussian distribution.
-                    float alpha = atan2(screenPos.y, screenPos.x);
-                    //float r =  sqrt(pow(x * scaleFactor.x, 2.0) + pow(y * scaleFactor.y, 2.0));
-                    float widthScale = min(scaleFactor.x, scaleFactor.y);
-                    float heightScale = max(scaleFactor.x, scaleFactor.y);
-                    float sigmaWidth = sigma / widthScale;
-                    float sigmaHeight = sigma / heightScale;
+                    float alpha = atan(sPoint.y - screenOrigo.y, sPoint.x - screenOrigo.x);
+                    //alpha = M_PI / 4;
+                    float sigmaWidth = sigma * (1 + length(scaleFactor));
+                    float sigmaHeight = sigma;
+
                     float a = pow(cos(alpha), 2.0) / (2 * pow(sigmaWidth, 2.0)) 
-                        + pow(sin(alpha), 2.0) / (2 * pow(sigmaHeight, 2.0));
+                        + pow(sin(alpha), 2.0) / (2 * pow(sigmaHeight, 2.0)) ;
                     float b = sin(2 * alpha) / (4 * pow(sigmaWidth, 2.0)) 
-                        - sin(2 * alpha) / (4 * pow(sigmaHeight, 2.0));
-                    float c = pow(sin(alpha), 2.0) / (2 * pow(sigmaWidth, 2.0)) 
-                        + pow(cos(alpha), 2.0) / (2 * pow(sigmaHeight, 2.0));
-                    intensity += sIntensity.rgb * exp(-( a * pow(x * scaleFactor.x, 2.0) 
-                        + 2 * b * x * scaleFactor.x * y * scaleFactor.y
-                        + c * pow(y * scaleFactor.y, 2.0) )) / filterSize;
+                        - sin(2 * alpha) / (4 * pow(sigmaHeight, 2.0)) ;
+                    float c = pow(sin(alpha), 2.0) / (2 * pow(sigmaWidth, 2.0))  
+                        + pow(cos(alpha), 2.0) / (2 * pow(sigmaHeight, 2.0)) ;
+                    intensity += sIntensity.rgb * exp(-( a * pow(x / (1 + length(sigmaWidth)), 2.0)
+                        + 2 * b * x / (1 + length(sigmaWidth)) * y / (1 + length(sigmaHeight))
+                        + c * pow(y / (1 + length(sigmaHeight)), 2.0) )) / newFilterSize;
                 }
             }
         }
@@ -105,7 +120,7 @@ Fragment getFragment() {
     if (length(intensity) < 0.01) {
         discard;
     }
-    //color = vec4(vec2(newFilterSize) / 14.0, 1.0, 1.0f);
+
     color = vec4(intensity, 1.0f);
 
     // Use the following to check for any intensity at all.
