e7250df205dd0bd1f3115f3b3227e16f69409858
Jonathan Grangien <jonathan.grangien@gmail.com>
2018-06-04 15:20:57 -0400

    Move toggle data loader files

--- modules/webgui/web/src/components/BottomBar/BottomBar.jsx
+++ modules/webgui/web/src/components/BottomBar/BottomBar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import OriginPicker from './Origin/OriginPicker';
 import TimePicker from './TimePicker';
 import TfEditor from './TfEditor/containers/TfEditor';
-import ToggleDataLoader from './DataLoader/ToggleDataLoader';
+import ToggleDataLoader from '../DataLoader/ToggleDataLoader';
 import styles from './BottomBar.scss';
 
 const BottomBar = () => (
--- modules/webgui/web/src/components/DataLoader/ToggleDataLoader.jsx
+++ modules/webgui/web/src/components/DataLoader/ToggleDataLoader.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { toggleActivated } from '../../../api/Actions/dataLoaderActions';
-import TabMenuItem from '../../common/TabMenu/TabMenuItem';
-import Label from '../../common/Label/Label';
+import { setActivated } from '../../api/Actions/dataLoaderActions';
+import TabMenuItem from '../common/TabMenu/TabMenuItem';
+import Label from '../common/Label/Label';
 
 import styles from './ToggleDataLoader.scss';
 
@@ -11,7 +11,7 @@ let ToggleDataLoader = (props) => {
   const { activated } = props;
 
   return (
-    <TabMenuItem active={activated} onClick={() => props.toggleActivated(!activated)}>
+    <TabMenuItem active={activated} onClick={() => props.setActivated(!activated)}>
       <div className={`${styles.container}`}>
         <Label size='medium'>DATA LOADER</Label>
       </div>
