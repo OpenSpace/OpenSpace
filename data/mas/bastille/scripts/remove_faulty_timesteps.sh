20b97e90e0cd3d503fa2978d1d661d7c3042e406
Jonathan Grangien <jonathan.grangien@gmail.com>
2018-06-13 16:02:28 -0400

    Put scripts in its own folder and add remove timesteps script

---
+++ data/mas/bastille/scripts/remove_faulty_timesteps.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+rm mas_merged_step_102.*
+rm mas_merged_step_104.*
+rm mas_merged_step_105.*
+rm mas_merged_step_106.*
+rm mas_merged_step_107.*
+rm mas_merged_step_108.*
+rm mas_merged_step_114.*
+rm mas_merged_step_116.*
+
--- data/mas/bastille/scripts/set_manual_time.sh
+++ data/mas/bastille/scripts/set_manual_time.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 # $1: timelist_ut_bastille_day.txt remove first line
+#
+# For every dictionary file 1 to LENGTH,
+# Set MaxValue to 1,
+# MinValue to 0,
+# Time to corresponding time for sequence number in timelist_ut_bastille_day.txt
 
-LENGTH=276
+LENGTH=117
 
 for i in $(seq 1 $LENGTH); do
   filename="/home/jgrangien/Code/OpenSpace/data/mas/bastille/generated/mas_merged_step_${i}.dictionary"
+
+  if [ ! -f /tmp/foo.txt ]; then
+    echo "Skipping file ${i}, not found."
+    continue
+  fi
 
   sed -i -e 's/MaxValue=[A-Za-z0-9.-]*/MaxValue=1/g' $filename
   sed -i -e 's/MinValue=[A-Za-z0-9.-]*/MinValue=0/g' $filename
