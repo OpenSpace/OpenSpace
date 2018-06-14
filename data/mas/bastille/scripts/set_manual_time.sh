ca974b1725fdac1778f69d75298f9abf35a5c1bd
Jonathan Grangien <jonathan.grangien@gmail.com>
2018-06-14 10:30:00 -0400

    Fix set time script

--- data/mas/bastille/scripts/set_manual_time.sh
+++ data/mas/bastille/scripts/set_manual_time.sh
@@ -19,6 +19,6 @@ for i in $(seq 1 $LENGTH); do
   sed -i -e 's/MaxValue=[A-Za-z0-9.-]*/MaxValue=1/g' $filename
   sed -i -e 's/MinValue=[A-Za-z0-9.-]*/MinValue=0/g' $filename
 
-  time=$(sed -n ${i}p "$1" | grep -o -P "2000.-*$")
+  time=$(sed -n ${i}p "$1" | grep -o -P "2000-.*$")
   sed -i -e "s/Time=\"0000-01-01T00:00:00.001\"/Time=\"${time}\"/g" $filename
 done
