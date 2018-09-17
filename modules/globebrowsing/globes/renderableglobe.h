4ff1f6e255989dff097b4451a59b6e63bfcd4c6b
Alexander Bock <mail@alexanderbock.eu>
2018-09-17 18:07:09 -0400

    Removal of ChunkRenderer

--- modules/globebrowsing/globes/renderableglobe.h
+++ modules/globebrowsing/globes/renderableglobe.h
@@ -45,6 +45,13 @@ namespace openspace::globebrowsing {
     struct Geodetic2;
     class LayerManager;
     class RenderableGlobe;
+    class Chunk;
+    class Ellipsoid;
+    class Grid;
+    class GPULayerManager;
+    class LayerManager;
+    class LayerShaderManager;
+    class RenderableGlobe;
 
 class PointGlobe;
 class LayerManager;
@@ -216,9 +223,6 @@ private:
     // Covers all positive longitudes
     std::unique_ptr<ChunkNode> _rightRoot;
 
-    // the patch used for actual rendering
-    std::unique_ptr<ChunkRenderer> _renderer;
-
     std::vector<std::unique_ptr<culling::ChunkCuller>> _chunkCullers;
 
     std::unique_ptr<chunklevelevaluator::Evaluator> _chunkEvaluatorByAvailableTiles;
@@ -226,6 +230,55 @@ private:
     std::unique_ptr<chunklevelevaluator::Evaluator> _chunkEvaluatorByDistance;
 
     bool _shadersNeedRecompilation = true;
+
+
+
+
+    // ChunkRenderer
+    void renderChunk(const Chunk& chunk, const RenderData& data);
+    /**
+ * Chunks can be rendered either globally or locally. Global rendering is performed
+ * in the model space of the globe. With global rendering, the vertex positions
+ * of a chunk are calculated in the vertex shader by transforming the geodetic
+ * coordinates of the chunk to model space coordinates. We can only achieve floating
+ * point precision by doing this which means that the camera too close to a global
+ * tile will lead to jagging. We only render global chunks for lower chunk levels.
+ */
+    void renderChunkGlobally(const Chunk& chunk, const RenderData& data);
+
+    /**
+     * Local rendering of chunks are done using linear interpolation in camera space.
+     * All four corner points of the chunk are calculated in double precision on the
+     * CPU and transformed to camera space with double precision matrix transforms.
+     * These positions can then be cast to floats and uploaded to the vertex shader.
+     * The vertex shader rendering performs linear interpolation between the four
+     * corner points to get the resulting chunk. This means that there will be an error
+     * due to the curvature of the globe. The smaller the patch is (with higher chunk
+     * levels) the better the approximation becomes. This is why we only render local
+     * chunks for higher chunk levels.
+     */
+    void renderChunkLocally(const Chunk& chunk, const RenderData& data);
+
+    ghoul::opengl::ProgramObject* getActivatedProgramWithTileData(
+        LayerShaderManager& layeredShaderManager, GPULayerManager& gpuLayerManager,
+        const Chunk& chunk);
+
+    void calculateEclipseShadows(const Chunk& chunk,
+        ghoul::opengl::ProgramObject* programObject, const RenderData& data);
+
+    void setCommonUniforms(ghoul::opengl::ProgramObject& programObject,
+        const Chunk& chunk, const RenderData& data);
+
+    std::shared_ptr<Grid> _grid;
+
+
+    // Two different shader programs. One for global and one for local rendering.
+    std::shared_ptr<LayerShaderManager> _globalLayerShaderManager;
+    std::shared_ptr<LayerShaderManager> _localLayerShaderManager;
+
+    // Layered texture uniforms are chached in the uniform ID handles.
+    std::shared_ptr<GPULayerManager> _globalGpuLayerManager;
+    std::shared_ptr<GPULayerManager> _localGpuLayerManager;
 };
 
 } // namespace openspace::globebrowsing
