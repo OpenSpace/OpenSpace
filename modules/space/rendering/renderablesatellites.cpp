ad448a841fc454c1337797c7e7407b79c2832c56
Elon <olssonelon@gmail.com>
2019-03-22 10:54:14 -0600

    some code to test 1 sgn, more renderables

--- modules/space/rendering/renderablesatellites.cpp
+++ modules/space/rendering/renderablesatellites.cpp
@@ -21,13 +21,8 @@
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE  *
  * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                         *
  ****************************************************************************************/
-#include <fstream>
-#include <chrono>
-#include <vector>
-
 
 #include <modules/space/rendering/renderablesatellites.h>
-#include <modules/space/translation/keplertranslation.h>
 
 #include <modules/base/basemodule.h>
 
@@ -38,11 +33,7 @@
 #include <openspace/util/updatestructures.h>
 
 #include <ghoul/filesystem/filesystem.h>
-#include <ghoul/filesystem/file.h>
 #include <ghoul/misc/csvreader.h>
-
-#include <fstream>
-
 
 // Todo:
 // Parse epoch correctly
@@ -52,7 +43,7 @@
 namespace {
     constexpr const char* ProgramName = "KeplerTrails";
     constexpr const char* KeyFile = "File";
-    constexpr const char* KeyLineNum = "LineNumber";
+    constexpr const char* KeyLineNumber = "LineNumber";
 
     
     static const openspace::properties::Property::PropertyInfo PathInfo = {
@@ -237,15 +228,210 @@ RenderableSatellites::RenderableSatellites(const ghoul::Dictionary& dictionary)
 * test
 */
 
-    const std::string& file = dictionary.value<std::string>(KeyFile);
+    const std::string& file = dictionary.value<str::string(KeyFile);
     int lineNumber = 1;
-    if (dictionary.hasKeyAndValue<double>(KeyLineNumber)) {
+    if (dictionary.hasKeyAndValue)<double>(KeyLineNumber) {
         lineNumber = static_cast<int>(dictionary.value<double>(KeyLineNumber));
     }
-    readTLEFile(file, lineNum);
-    }
+    readTLEFile(file, lineNumber);
+
 }
-    // The list of leap years only goes until 2056 as we need to touch this file then
+
+void RenderableSatellites::readTLEFile(const std::string& filename, int lineNumber){
+    ghoul_assert(FileSys.fileExists(filename), "The filename must exist");
+
+    std::ifstream file;
+    file.exceptions(std::ofstream::failbit | std::ofstream::badbit);
+    file.open(filename);
+
+    // All of the Kepler element information
+    struct {
+        double inclination = 0.0;
+        double semiMajorAxis = 0.0;
+        double ascendingNode = 0.0;
+        double eccentricity = 0.0;
+        double argumentOfPeriapsis = 0.0;
+        double meanAnomaly = 0.0;
+        double meanMotion = 0.0;
+        double epoch = 0.0;
+    } keplerElements;
+
+    std::string line;
+    // Loop through and throw out lines until getting to the linNum of interest
+    for (int i = 1; i < lineNum; ++i) {
+        std::getline(file, line);
+    }
+    std::getline(file, line); // Throw out the TLE title line (1st)
+
+    std::getline(file, line); // Get line 1 of TLE format
+    if (line[0] == '1') {
+        // First line
+        // Field Columns   Content
+        //     1   01-01   Line number
+        //     2   03-07   Satellite number
+        //     3   08-08   Classification (U = Unclassified)
+        //     4   10-11   International Designator (Last two digits of launch year)
+        //     5   12-14   International Designator (Launch number of the year)
+        //     6   15-17   International Designator(piece of the launch)    A
+        //     7   19-20   Epoch Year(last two digits of year)
+        //     8   21-32   Epoch(day of the year and fractional portion of the day)
+        //     9   34-43   First Time Derivative of the Mean Motion divided by two
+        //    10   45-52   Second Time Derivative of Mean Motion divided by six
+        //    11   54-61   BSTAR drag term(decimal point assumed)[10] - 11606 - 4
+        //    12   63-63   The "Ephemeris type"
+        //    13   65-68   Element set  number.Incremented when a new TLE is generated
+        //    14   69-69   Checksum (modulo 10)
+        keplerElements.epoch = epochFromSubstring(line.substr(18, 14));
+    } else {
+        throw ghoul::RuntimeError(fmt::format(
+            "File {} @ line {} does not have '1' header", filename, lineNum + 1
+        ));
+    }
+
+    std::getline(file, line); // Get line 2 of TLE format
+     if (line[0] == '2') {
+        // Second line
+        // Field    Columns   Content
+        //     1      01-01   Line number
+        //     2      03-07   Satellite number
+        //     3      09-16   Inclination (degrees)
+        //     4      18-25   Right ascension of the ascending node (degrees)
+        //     5      27-33   Eccentricity (decimal point assumed)
+        //     6      35-42   Argument of perigee (degrees)
+        //     7      44-51   Mean Anomaly (degrees)
+        //     8      53-63   Mean Motion (revolutions per day)
+        //     9      64-68   Revolution number at epoch (revolutions)
+        //    10      69-69   Checksum (modulo 10)
+
+        std::stringstream stream;
+        stream.exceptions(std::ios::failbit);
+
+        // Get inclination
+        stream.str(line.substr(8, 8));
+        stream >> keplerElements.inclination;
+        stream.clear();
+
+        // Get Right ascension of the ascending node
+        stream.str(line.substr(17, 8));
+        stream >> keplerElements.ascendingNode;
+        stream.clear();
+
+        // Get Eccentricity
+        stream.str("0." + line.substr(26, 7));
+        stream >> keplerElements.eccentricity;
+        stream.clear();
+
+        // Get argument of periapsis
+        stream.str(line.substr(34, 8));
+        stream >> keplerElements.argumentOfPeriapsis;
+        stream.clear();
+
+        // Get mean anomaly
+        stream.str(line.substr(43, 8));
+        stream >> keplerElements.meanAnomaly;
+        stream.clear();
+
+        // Get mean motion
+        stream.str(line.substr(52, 11));
+        stream >> keplerElements.meanMotion;
+    } else {
+        throw ghoul::RuntimeError(fmt::format(
+            "File {} @ line {} does not have '2' header", filename, lineNum + 2
+        ));
+    }
+    file.close();
+
+    // Calculate the semi major axis based on the mean motion using kepler's laws
+    keplerElements.semiMajorAxis = calculateSemiMajorAxis(keplerElements.meanMotion);
+
+    // Converting the mean motion (revolutions per day) to period (seconds per revolution)
+    using namespace std::chrono;
+    double period = seconds(hours(24)).count() / keplerElements.meanMotion;
+
+    setKeplerElements(
+        keplerElements.eccentricity,
+        keplerElements.semiMajorAxis,
+        keplerElements.inclination,
+        keplerElements.ascendingNode,
+        keplerElements.argumentOfPeriapsis,
+        keplerElements.meanAnomaly,
+        period,
+        keplerElements.epoch
+    );
+
+}
+
+double epochFromSubstring(const std::string& epochString) {
+        // The epochString is in the form:
+        // YYDDD.DDDDDDDD
+        // With YY being the last two years of the launch epoch, the first DDD the day
+        // of the year and the remaning a fractional part of the day
+
+        // The main overview of this function:
+        // 1. Reconstruct the full year from the YY part
+        // 2. Calculate the number of seconds since the beginning of the year
+        // 2.a Get the number of full days since the beginning of the year
+        // 2.b If the year is a leap year, modify the number of days
+        // 3. Convert the number of days to a number of seconds
+        // 4. Get the number of leap seconds since January 1st, 2000 and remove them
+        // 5. Adjust for the fact the epoch starts on 1st Januaray at 12:00:00, not
+        // midnight
+
+        // According to https://celestrak.com/columns/v04n03/
+        // Apparently, US Space Command sees no need to change the two-line element
+        // set format yet since no artificial earth satellites existed prior to 1957.
+        // By their reasoning, two-digit years from 57-99 correspond to 1957-1999 and
+        // those from 00-56 correspond to 2000-2056. We'll see each other again in 2057!
+
+        // 1. Get the full year
+        std::string yearPrefix = [y = epochString.substr(0, 2)](){
+            int year = std::atoi(y.c_str());
+            return year >= 57 ? "19" : "20";
+        }();
+        const int year = std::atoi((yearPrefix + epochString.substr(0, 2)).c_str());
+        const int daysSince2000 = countDays(year);
+
+        // 2.
+        // 2.a
+        double daysInYear = std::atof(epochString.substr(2).c_str());
+
+        // 2.b
+        const bool isInLeapYear = std::find(
+            LeapYears.begin(),
+            LeapYears.end(),
+            year
+        ) != LeapYears.end();
+        if (isInLeapYear && daysInYear >= 60) {
+            // We are in a leap year, so we have an effective day more if we are
+            // beyond the end of february (= 31+29 days)
+            --daysInYear;
+        }
+
+        // 3
+        using namespace std::chrono;
+        const int SecondsPerDay = static_cast<int>(seconds(hours(24)).count());
+        //Need to subtract 1 from daysInYear since it is not a zero-based count
+        const double nSecondsSince2000 = (daysSince2000 + daysInYear - 1) * SecondsPerDay;
+
+        // 4
+        // We need to remove additionbal leap seconds past 2000 and add them prior to
+        // 2000 to sync up the time zones
+        const double nLeapSecondsOffset = -countLeapSeconds(
+            year,
+            static_cast<int>(std::floor(daysInYear))
+        );
+
+        // 5
+        const double nSecondsEpochOffset = static_cast<double>(
+            seconds(hours(12)).count()
+        );
+
+        // Combine all of the values
+        const double epoch = nSecondsSince2000 + nLeapSecondsOffset - nSecondsEpochOffset;
+        return epoch;
+    }
+
+      // The list of leap years only goes until 2056 as we need to touch this file then
     // again anyway ;)
     const std::vector<int> LeapYears = {
         1956, 1960, 1964, 1968, 1972, 1976, 1980, 1984, 1988, 1992, 1996,
@@ -366,201 +552,6 @@ RenderableSatellites::RenderableSatellites(const ghoul::Dictionary& dictionary)
         // We need the semi major axis in km instead of m
         return semiMajorAxis / 1000.0;
     }
-
-double epochFromSubstring(const std::string& epochString) {
-        // The epochString is in the form:
-        // YYDDD.DDDDDDDD
-        // With YY being the last two years of the launch epoch, the first DDD the day
-        // of the year and the remaning a fractional part of the day
-
-        // The main overview of this function:
-        // 1. Reconstruct the full year from the YY part
-        // 2. Calculate the number of seconds since the beginning of the year
-        // 2.a Get the number of full days since the beginning of the year
-        // 2.b If the year is a leap year, modify the number of days
-        // 3. Convert the number of days to a number of seconds
-        // 4. Get the number of leap seconds since January 1st, 2000 and remove them
-        // 5. Adjust for the fact the epoch starts on 1st Januaray at 12:00:00, not
-        // midnight
-
-        // According to https://celestrak.com/columns/v04n03/
-        // Apparently, US Space Command sees no need to change the two-line element
-        // set format yet since no artificial earth satellites existed prior to 1957.
-        // By their reasoning, two-digit years from 57-99 correspond to 1957-1999 and
-        // those from 00-56 correspond to 2000-2056. We'll see each other again in 2057!
-
-        // 1. Get the full year
-        std::string yearPrefix = [y = epochString.substr(0, 2)](){
-            int year = std::atoi(y.c_str());
-            return year >= 57 ? "19" : "20";
-        }();
-        const int year = std::atoi((yearPrefix + epochString.substr(0, 2)).c_str());
-        const int daysSince2000 = countDays(year);
-
-        // 2.
-        // 2.a
-        double daysInYear = std::atof(epochString.substr(2).c_str());
-
-        // 2.b
-        const bool isInLeapYear = std::find(
-            LeapYears.begin(),
-            LeapYears.end(),
-            year
-        ) != LeapYears.end();
-        if (isInLeapYear && daysInYear >= 60) {
-            // We are in a leap year, so we have an effective day more if we are
-            // beyond the end of february (= 31+29 days)
-            --daysInYear;
-        }
-
-        // 3
-        using namespace std::chrono;
-        const int SecondsPerDay = static_cast<int>(seconds(hours(24)).count());
-        //Need to subtract 1 from daysInYear since it is not a zero-based count
-        const double nSecondsSince2000 = (daysSince2000 + daysInYear - 1) * SecondsPerDay;
-
-        // 4
-        // We need to remove additionbal leap seconds past 2000 and add them prior to
-        // 2000 to sync up the time zones
-        const double nLeapSecondsOffset = -countLeapSeconds(
-            year,
-            static_cast<int>(std::floor(daysInYear))
-        );
-
-        // 5
-        const double nSecondsEpochOffset = static_cast<double>(
-            seconds(hours(12)).count()
-        );
-
-        // Combine all of the values
-        const double epoch = nSecondsSince2000 + nLeapSecondsOffset - nSecondsEpochOffset;
-        return epoch;
-    }
-    
-void RenderableSatellites::readTLEFile(const std::string& filename, int lineNum){
-    ghoul_assert(FileSys.fileExists(filename), "The filename must exist");
-
-    std::ifstream file;
-    file.exceptions(std::ofstream::failbit | std::ofstream::badbit);
-    file.open(filename);
-
-    // All of the Kepler element information
-    struct {
-        double inclination = 0.0;
-        double semiMajorAxis = 0.0;
-        double ascendingNode = 0.0;
-        double eccentricity = 0.0;
-        double argumentOfPeriapsis = 0.0;
-        double meanAnomaly = 0.0;
-        double meanMotion = 0.0;
-        double epoch = 0.0;
-    } keplerElements;
-
-    std::string line;
-    // Loop through and throw out lines until getting to the linNum of interest
-    for (int i = 1; i < lineNum; ++i) {
-        std::getline(file, line);
-    }
-    std::getline(file, line); // Throw out the TLE title line (1st)
-
-    std::getline(file, line); // Get line 1 of TLE format
-    if (line[0] == '1') {
-        // First line
-        // Field Columns   Content
-        //     1   01-01   Line number
-        //     2   03-07   Satellite number
-        //     3   08-08   Classification (U = Unclassified)
-        //     4   10-11   International Designator (Last two digits of launch year)
-        //     5   12-14   International Designator (Launch number of the year)
-        //     6   15-17   International Designator(piece of the launch)    A
-        //     7   19-20   Epoch Year(last two digits of year)
-        //     8   21-32   Epoch(day of the year and fractional portion of the day)
-        //     9   34-43   First Time Derivative of the Mean Motion divided by two
-        //    10   45-52   Second Time Derivative of Mean Motion divided by six
-        //    11   54-61   BSTAR drag term(decimal point assumed)[10] - 11606 - 4
-        //    12   63-63   The "Ephemeris type"
-        //    13   65-68   Element set  number.Incremented when a new TLE is generated
-        //    14   69-69   Checksum (modulo 10)
-        keplerElements.epoch = epochFromSubstring(line.substr(18, 14));
-    } else {
-        throw ghoul::RuntimeError(fmt::format(
-            "File {} @ line {} does not have '1' header", filename, lineNum + 1
-        ));
-    }
-
-    std::getline(file, line); // Get line 2 of TLE format
-     if (line[0] == '2') {
-        // Second line
-        // Field    Columns   Content
-        //     1      01-01   Line number
-        //     2      03-07   Satellite number
-        //     3      09-16   Inclination (degrees)
-        //     4      18-25   Right ascension of the ascending node (degrees)
-        //     5      27-33   Eccentricity (decimal point assumed)
-        //     6      35-42   Argument of perigee (degrees)
-        //     7      44-51   Mean Anomaly (degrees)
-        //     8      53-63   Mean Motion (revolutions per day)
-        //     9      64-68   Revolution number at epoch (revolutions)
-        //    10      69-69   Checksum (modulo 10)
-
-        std::stringstream stream;
-        stream.exceptions(std::ios::failbit);
-
-        // Get inclination
-        stream.str(line.substr(8, 8));
-        stream >> keplerElements.inclination;
-        stream.clear();
-
-        // Get Right ascension of the ascending node
-        stream.str(line.substr(17, 8));
-        stream >> keplerElements.ascendingNode;
-        stream.clear();
-
-        // Get Eccentricity
-        stream.str("0." + line.substr(26, 7));
-        stream >> keplerElements.eccentricity;
-        stream.clear();
-
-        // Get argument of periapsis
-        stream.str(line.substr(34, 8));
-        stream >> keplerElements.argumentOfPeriapsis;
-        stream.clear();
-
-        // Get mean anomaly
-        stream.str(line.substr(43, 8));
-        stream >> keplerElements.meanAnomaly;
-        stream.clear();
-
-        // Get mean motion
-        stream.str(line.substr(52, 11));
-        stream >> keplerElements.meanMotion;
-    } else {
-        throw ghoul::RuntimeError(fmt::format(
-            "File {} @ line {} does not have '2' header", filename, lineNum + 2
-        ));
-    }
-    file.close();
-
-    // Calculate the semi major axis based on the mean motion using kepler's laws
-    keplerElements.semiMajorAxis = calculateSemiMajorAxis(keplerElements.meanMotion);
-
-    // Converting the mean motion (revolutions per day) to period (seconds per revolution)
-    using namespace std::chrono;
-    double period = seconds(hours(24)).count() / keplerElements.meanMotion;
-
-    // setKeplerElements(
-    //     keplerElements.eccentricity,
-    //     keplerElements.semiMajorAxis,
-    //     keplerElements.inclination,
-    //     keplerElements.ascendingNode,
-    //     keplerElements.argumentOfPeriapsis,
-    //     keplerElements.meanAnomaly,
-    //     period,
-    //     keplerElements.epoch
-    // );
-
-}
-
 /*
 * !test
 */
