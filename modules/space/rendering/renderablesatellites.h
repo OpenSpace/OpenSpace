b96bea432791e135e9955ff89601ec4000d80ef4
Elon <olssonelon@gmail.com>
2019-03-22 10:54:14 -0600

    some code to test 1 sgn, more renderables

--- modules/space/rendering/renderablesatellites.h
+++ modules/space/rendering/renderablesatellites.h
@@ -27,7 +27,7 @@
 #include <modules/base/rendering/renderabletrail.h>
 #include <modules/space/translation/keplertranslation.h>
 
-// #include <openspace/util/circlegeometry.h>
+#include <openspace/util/circlegeometry.h>
 #include <openspace/properties/stringproperty.h>
 #include <openspace/properties/scalar/uintproperty.h>
 
@@ -52,12 +52,6 @@ public:
     
     void render(const RenderData& data, RendererTasks& rendererTask) override;
     void update(const UpdateData& data) override;
-
-    /*
-    void setKeplerElements(double eccentricity, double semiMajorAxis, double inclination,
-        double ascendingNode, double argumentOfPeriapsis, double meanAnomalyAtEpoch,
-        double orbitalPeriod, double epoch);
-    */
     
     static documentation::Documentation Documentation();
 
