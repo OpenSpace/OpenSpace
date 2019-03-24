bb3db7ada704105b2b8279461f084d19372477d4
Alexander Bock <mail@alexanderbock.eu>
2019-03-24 11:19:39 +0100

    Feature/jenkins fix (#816)
    * Cleanup
    * CMake cleanups
    * Update current year
    * Update copyright header
    * Use script to return list of all modules
    * Update credits, license and ghoul

--- modules/webbrowser/src/browserinstance.cpp
+++ modules/webbrowser/src/browserinstance.cpp
@@ -2,7 +2,7 @@
  *                                                                                       *
  * OpenSpace                                                                             *
  *                                                                                       *
- * Copyright (c) 2014-2018                                                               *
+ * Copyright (c) 2014-2019                                                               *
  *                                                                                       *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this  *
  * software and associated documentation files (the "Software"), to deal in the Software *
@@ -50,7 +50,7 @@ BrowserInstance::BrowserInstance(WebRenderHandler* renderer,
 
     CefWindowInfo windowInfo;
     const bool renderTransparent = true;
-    windowInfo.SetAsWindowless(nullptr, renderTransparent);
+    windowInfo.SetAsWindowless(0, renderTransparent);
 
     CefBrowserSettings browserSettings;
     browserSettings.windowless_frame_rate = 60;
