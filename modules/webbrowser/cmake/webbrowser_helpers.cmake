d3a39f7f86c4b0afb8e8d202e80f34e6509d736d
Emil Axelsson <nils.emil.axelsson@gmail.com>
2019-05-18 19:29:02 +0200

    Try to fix cmake issue with single-configuration generators

--- modules/webbrowser/cmake/webbrowser_helpers.cmake
+++ modules/webbrowser/cmake/webbrowser_helpers.cmake
@@ -62,7 +62,7 @@ function(run_cef_platform_config cef_root cef_target module_path)
 endfunction()
 
 function(run_cef_macosx_config CEF_ROOT module_path)
-    set(CEF_FINAL_APP "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/Debug/${CEF_TARGET}.app")
+    set(CEF_FINAL_APP "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${CMAKE_BUILD_TYPE}/${CEF_TARGET}.app")
     set(CEF_FINAL_HELPER_APP "${CEF_FINAL_APP}/Contents/Frameworks/${CEF_HELPER_TARGET}.app")
     set(CEF_FRAMEWORK_LOCATION "${CEF_BINARY_DIR}/Chromium Embedded Framework.framework")
     set(CEF_FRAMEWORK_FINAL_LOCATION "${CEF_FINAL_APP}/Contents/Frameworks/Chromium Embedded Framework.framework")
@@ -80,7 +80,7 @@ function(run_cef_macosx_config CEF_ROOT module_path)
         TARGET ${CEF_TARGET}
         POST_BUILD
         # Copy the helper app bundle into the Frameworks directory.
-        COMMAND ${CMAKE_COMMAND} -E copy_directory "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/Debug${CEF_HELPER_APP}" "${CEF_FINAL_HELPER_APP}"
+        COMMAND ${CMAKE_COMMAND} -E copy_directory "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${CMAKE_BUILD_TYPE}${CEF_HELPER_APP}" "${CEF_FINAL_HELPER_APP}"
         # Copy the CEF framework into the Frameworks directory.
         COMMAND ${CMAKE_COMMAND} -E copy_directory "${CEF_FRAMEWORK_LOCATION}" "${CEF_FRAMEWORK_FINAL_LOCATION}"
         VERBATIM
