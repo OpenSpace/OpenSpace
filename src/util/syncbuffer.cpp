517e2953ccafd3ec2ce3b39e2c8cf910958da034
GPayne <payne.gene@gmail.com>
2019-07-17 15:54:31 -0600

    Removed unnecessary asserts and added sync buffer overflow check to encode template

--- src/util/syncbuffer.cpp
+++ src/util/syncbuffer.cpp
@@ -35,8 +35,6 @@ SyncBuffer::SyncBuffer(size_t n)
 SyncBuffer::~SyncBuffer() {} // NOLINT
 
 void SyncBuffer::encode(const std::string& s) {
-    ghoul_assert(_encodeOffset + sizeof(char) * s.size() + sizeof(int32_t) < _n, "");
-
     int32_t anticpatedBufferSize = _encodeOffset + (sizeof(char) * s.size())
         + sizeof(int32_t);
     if (anticpatedBufferSize >= _n) {
