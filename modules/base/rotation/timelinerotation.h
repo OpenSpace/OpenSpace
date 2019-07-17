c97d5126eced98f43bd6ba57b60cb7df6a9d2052
Emil Axelsson <emil.axelsson@liu.se>
2019-07-17 15:21:13 +0200

    Feature/timeline translation (#931)
    * Work on timeline translations
    * Add Apollo 11 Descent data
    * Ability to display negative altitudes
    * Expose globe translation properties
    * Added timeline rotation, and some scene updates. 
    * Cleaning up apollo_sites scene; fixing boulder 1 in place to adress heightmap issue
    * Use quaternion slerp instaet of linear interpolation + Cleanup
    * Change to old lem model while waiting for new version to be pushed to data repo
    * Small fixes

--- modules/base/rotation/timelinerotation.h
+++ modules/base/rotation/timelinerotation.h
@@ -22,81 +22,28 @@
  * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                         *
  ****************************************************************************************/
 
-#ifndef __OPENSPACE_CORE___TIMELINE___H__
-#define __OPENSPACE_CORE___TIMELINE___H__
+#ifndef __OPENSPACE_MODULE_BASE___TIMELINEROTATION___H__
+#define __OPENSPACE_MODULE_BASE___TIMELINEROTATION___H__
 
-#include <algorithm>
-#include <deque>
-#include <cstddef>
+#include <openspace/scene/rotation.h>
+#include <openspace/util/timeline.h>
 
 namespace openspace {
 
-/**
-* Base class for keyframes
-*/
-struct KeyframeBase {
-    size_t id;
-    double timestamp;
-};
+struct UpdateData;
 
-/**
-* Templated class for keyframes containing data
-*/
-template <typename T>
-struct Keyframe : public KeyframeBase {
-    Keyframe(size_t i, double t, T d);
+namespace documentation { struct Documentation; }
 
-    Keyframe(Keyframe const&) = default;
-    Keyframe(Keyframe&&) = default;
-    Keyframe& operator=(Keyframe&&) = default;
-    Keyframe& operator=(Keyframe const&) = default;
-    T data;
-};
-
-/**
-* Templated class for timelines
-*/
-template <typename T>
-class Timeline {
+class TimelineRotation : public Rotation {
 public:
-    virtual ~Timeline() = default;
-
-    void addKeyframe(double time, const T& data);
-    void addKeyframe(double time, T&& data);
-    void clearKeyframes();
-    void removeKeyframe(size_t id);
-    void removeKeyframesBefore(double timestamp, bool inclusive = false);
-    void removeKeyframesAfter(double timestamp, bool inclusive = false);
-    void removeKeyframesBetween(double begin, double end, bool inclusiveBegin = false,
-        bool inclusiveEnd = false);
-    size_t nKeyframes() const;
-    const Keyframe<T>* firstKeyframeAfter(double timestamp, bool inclusive = false) const;
-    const Keyframe<T>* lastKeyframeBefore(double timestamp, bool inclusive = false) const;
-
-    const std::deque<Keyframe<T>>& keyframes() const;
+    TimelineRotation(const ghoul::Dictionary& dictionary);
+    glm::dmat3 matrix(const UpdateData& data) const override;
+    static documentation::Documentation Documentation();
 
 private:
-    size_t _nextKeyframeId = 1;
-    std::deque<Keyframe<T>> _keyframes;
+    Timeline<std::unique_ptr<Rotation>> _timeline;
 };
-
-/**
-* Return true if the timestamp of a is smaller the timestamp of b.
-*/
-bool compareKeyframeTimes(const KeyframeBase& a, const KeyframeBase& b);
-
-/**
-* Return true if a is smaller than the timestamp of b.
-*/
-bool compareTimeWithKeyframeTime(double a, const KeyframeBase& b);
-
-/**
-* Return true if the timestamp of a is smaller than b.
-*/
-bool compareKeyframeTimeWithTime(const KeyframeBase& a, double b);
 
 } // namespace openspace
 
-#include "timeline.inl"
-
-#endif // __OPENSPACE_CORE___TIMELINE___H__
+#endif // __OPENSPACE_MODULE_BASE___TIMELINEROTATION___H__
--- modules/base/translation/timelinetranslation.h
+++ modules/base/translation/timelinetranslation.h
@@ -22,81 +22,29 @@
  * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                         *
  ****************************************************************************************/
 
-#ifndef __OPENSPACE_CORE___TIMELINE___H__
-#define __OPENSPACE_CORE___TIMELINE___H__
+#ifndef __OPENSPACE_MODULE_BASE___TIMELINETRANSLATION___H__
+#define __OPENSPACE_MODULE_BASE___TIMELINETRANSLATION___H__
 
-#include <algorithm>
-#include <deque>
-#include <cstddef>
+#include <openspace/scene/translation.h>
+#include <openspace/util/timeline.h>
 
 namespace openspace {
 
-/**
-* Base class for keyframes
-*/
-struct KeyframeBase {
-    size_t id;
-    double timestamp;
-};
+struct UpdateData;
 
-/**
-* Templated class for keyframes containing data
-*/
-template <typename T>
-struct Keyframe : public KeyframeBase {
-    Keyframe(size_t i, double t, T d);
+namespace documentation { struct Documentation; }
 
-    Keyframe(Keyframe const&) = default;
-    Keyframe(Keyframe&&) = default;
-    Keyframe& operator=(Keyframe&&) = default;
-    Keyframe& operator=(Keyframe const&) = default;
-    T data;
-};
-
-/**
-* Templated class for timelines
-*/
-template <typename T>
-class Timeline {
+class TimelineTranslation : public Translation {
 public:
-    virtual ~Timeline() = default;
+    TimelineTranslation(const ghoul::Dictionary& dictionary);
 
-    void addKeyframe(double time, const T& data);
-    void addKeyframe(double time, T&& data);
-    void clearKeyframes();
-    void removeKeyframe(size_t id);
-    void removeKeyframesBefore(double timestamp, bool inclusive = false);
-    void removeKeyframesAfter(double timestamp, bool inclusive = false);
-    void removeKeyframesBetween(double begin, double end, bool inclusiveBegin = false,
-        bool inclusiveEnd = false);
-    size_t nKeyframes() const;
-    const Keyframe<T>* firstKeyframeAfter(double timestamp, bool inclusive = false) const;
-    const Keyframe<T>* lastKeyframeBefore(double timestamp, bool inclusive = false) const;
-
-    const std::deque<Keyframe<T>>& keyframes() const;
+    glm::dvec3 position(const UpdateData& data) const override;
+    static documentation::Documentation Documentation();
 
 private:
-    size_t _nextKeyframeId = 1;
-    std::deque<Keyframe<T>> _keyframes;
+    Timeline<std::unique_ptr<Translation>> _timeline;
 };
-
-/**
-* Return true if the timestamp of a is smaller the timestamp of b.
-*/
-bool compareKeyframeTimes(const KeyframeBase& a, const KeyframeBase& b);
-
-/**
-* Return true if a is smaller than the timestamp of b.
-*/
-bool compareTimeWithKeyframeTime(double a, const KeyframeBase& b);
-
-/**
-* Return true if the timestamp of a is smaller than b.
-*/
-bool compareKeyframeTimeWithTime(const KeyframeBase& a, double b);
 
 } // namespace openspace
 
-#include "timeline.inl"
-
-#endif // __OPENSPACE_CORE___TIMELINE___H__
+#endif // __OPENSPACE_MODULE_BASE___TIMELINETRANSLATION___H__
