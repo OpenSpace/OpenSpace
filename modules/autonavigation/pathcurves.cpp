bf434ee3ee650bf1c1537ee4b7c99f58972caec5
Lingis <pringela@gmail.com>
2020-02-06 13:37:10 -0500

    Same anchor bug fix and readability in bezier3

--- modules/autonavigation/pathcurves.cpp
+++ modules/autonavigation/pathcurves.cpp
@@ -57,75 +57,77 @@ std::vector<glm::dvec3> PathCurve::getPoints() {
 }
 
 Bezier3Curve::Bezier3Curve(CameraState& start, CameraState& end) {
-    //TODO try to clean up using variable for nodes
     glm::dvec3 startNodePos = sceneGraphNode(start.referenceNode)->worldPosition();
     glm::dvec3 endNodePos = sceneGraphNode(end.referenceNode)->worldPosition();
-    glm::dvec3 startNodeToStartPos = start.position - startNodePos;
-    glm::dvec3 endNodeToEndPos = end.position - endNodePos;
     double startNodeRadius = sceneGraphNode(start.referenceNode)->boundingSphere();
     double endNodeRadius = sceneGraphNode(end.referenceNode)->boundingSphere();
 
-    glm::dvec3 startNodeToEndNode = endNodePos - startNodePos;
-    glm::dvec3 startToEnd = end.position - start.position;
-    double distance = length(startToEnd);
+    glm::dvec3 startNodeToStartPos = start.position - startNodePos;
+    glm::dvec3 endNodeToEndPos = end.position - endNodePos;
 
-    // Assuming we move straigh out to point to a distance proportional to radius, angle is enough to check collision risk
-    double cosStartAngle = glm::dot(normalize(startNodeToStartPos), normalize(startToEnd));
-    double cosEndAngle = glm::dot(normalize(endNodeToEndPos), normalize(startToEnd));
-    double cosStartDir = glm::dot(normalize(startNodeToStartPos), normalize(endNodeToEndPos));
-
-    //TODO: investigate suitable values, could be risky close to surface..
-    bool TARGET_BEHIND_STARTNODE = cosStartAngle < -0.8;
-    bool TARGET_BEHIND_ENDNODE = cosEndAngle > 0.8;
-    bool TARGET_IN_OPPOSITE_DIRECTION = cosStartAngle > 0.7;
-
-    // Variables that determine behaviour close to start and end
     double startTangentLength = 2.0 * startNodeRadius;
     double endTangentLength = 2.0 * endNodeRadius;
+    glm::dvec3 startTangentDirection = normalize(startNodeToStartPos);
+    glm::dvec3 endTangentDirection = normalize(endNodeToEndPos);
 
-    // SET CONTROL POINTS
     // Start by going outwards
     _points.push_back(start.position);
-    _points.push_back(start.position + startTangentLength * normalize(startNodeToStartPos));
+    _points.push_back(start.position + startTangentLength * startTangentDirection);
 
-    // Avoid collision with startnode by control points on the side of it
-    if (TARGET_BEHIND_STARTNODE) {
-        glm::dvec3 parallell = glm::proj(startNodeToStartPos, startNodeToEndNode);
-        glm::dvec3 orthogonal = normalize(startNodeToStartPos - parallell);
-        double dist = 5.0 * startNodeRadius;
-        glm::dvec3 extraKnot = startNodePos + dist * orthogonal;
+    if (start.referenceNode != end.referenceNode) {
 
-        _points.push_back(extraKnot + parallell);
-        _points.push_back(extraKnot);
-        _points.push_back(extraKnot - parallell);
+        glm::dvec3 startNodeToEndNode = endNodePos - startNodePos;
+        glm::dvec3 startToEndDirection = normalize(end.position - start.position);
+
+        // Assuming we move straigh out to point to a distance proportional to radius, angle is enough to check collision risk
+        double cosStartAngle = glm::dot(startTangentDirection, startToEndDirection);
+        double cosEndAngle = glm::dot(endTangentDirection, startToEndDirection);
+        double cosStartDir = glm::dot(startTangentDirection, endTangentDirection);
+
+        //TODO: investigate suitable values, could be risky close to surface..
+        bool TARGET_BEHIND_STARTNODE = cosStartAngle < -0.8;
+        bool TARGET_BEHIND_ENDNODE = cosEndAngle > 0.8;
+        bool TARGET_IN_OPPOSITE_DIRECTION = cosStartAngle > 0.7;
+
+        // Avoid collision with startnode by adding control points on the side of it
+        if (TARGET_BEHIND_STARTNODE) {
+            glm::dvec3 parallell = glm::proj(startNodeToStartPos, startNodeToEndNode);
+            glm::dvec3 orthogonal = normalize(startNodeToStartPos - parallell);
+            double dist = 5.0 * startNodeRadius;
+            glm::dvec3 extraKnot = startNodePos + dist * orthogonal;
+
+            _points.push_back(extraKnot + parallell);
+            _points.push_back(extraKnot);
+            _points.push_back(extraKnot - parallell);
+        }
+
+        // Zoom out, to get a better understanding in a 180 degree turn situation
+        if (TARGET_IN_OPPOSITE_DIRECTION) {
+            glm::dvec3 parallell = glm::proj(startNodeToStartPos, startNodeToEndNode);
+            glm::dvec3 orthogonal = normalize(startNodeToStartPos - parallell);
+            double dist = 0.5 * length(startNodeToEndNode);
+            // Distant middle point
+            glm::dvec3 extraKnot = startNodePos + dist * normalize(parallell) + 3.0 * dist * orthogonal;
+
+            _points.push_back(extraKnot - 0.3 * dist *  normalize(parallell));
+            _points.push_back(extraKnot);
+            _points.push_back(extraKnot + 0.3 * dist *  normalize(parallell));
+        }
+
+        // Avoid collision with endnode by adding control points on the side of it
+        if (TARGET_BEHIND_ENDNODE) {
+            glm::dvec3 parallell = glm::proj(endNodeToEndPos, startNodeToEndNode);
+            glm::dvec3 orthogonal = normalize(endNodeToEndPos - parallell);
+            double dist = 5.0 * endNodeRadius;
+            glm::dvec3 extraKnot = endNodePos + dist * orthogonal;
+
+            _points.push_back(extraKnot - parallell);
+            _points.push_back(extraKnot);
+            _points.push_back(extraKnot + parallell);
+        }
     }
 
-    // Zoom out, to get a better understanding in a 180 degree turn situation
-    if (TARGET_IN_OPPOSITE_DIRECTION) {
-        glm::dvec3 parallell = glm::proj(startNodeToStartPos, startNodeToEndNode);
-        glm::dvec3 orthogonal = normalize(startNodeToStartPos - parallell);
-        double dist = 0.5 * length(startNodeToEndNode);
-        // Distant middle point
-        glm::dvec3 extraKnot = startNodePos + dist * normalize(parallell) + 3.0 * dist * orthogonal;
-
-        _points.push_back(extraKnot - 0.3 * dist *  normalize(parallell));
-        _points.push_back(extraKnot);
-        _points.push_back(extraKnot + 0.3 * dist *  normalize(parallell));
-    }
-
-    // Avoid collision with endnode by adding a control points on the side of it
-    if (TARGET_BEHIND_ENDNODE) {
-        glm::dvec3 parallell = normalize(startNodeToEndNode) * glm::dot(endNodeToEndPos, normalize(startNodeToEndNode));
-        glm::dvec3 orthogonal = normalize(endNodeToEndPos - parallell);
-        double dist = 5.0 * endNodeRadius;
-        glm::dvec3 extraKnot = endNodePos + dist * orthogonal;
-
-        _points.push_back(extraKnot - parallell);
-        _points.push_back(extraKnot);
-        _points.push_back(extraKnot + parallell);
-    }
-
-    _points.push_back(end.position + endTangentLength * normalize(endNodeToEndPos));
+    _points.push_back(end.position + endTangentLength * endTangentDirection);
     _points.push_back(end.position);
 }
 
