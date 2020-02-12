324f33dbb82722186a94b33491da43d23c376859
Alexander Bock <mail@alexanderbock.eu>
2020-02-12 10:58:36 +0100

    Small cleanups

--- modules/space/rendering/renderablesatellites.h
+++ modules/space/rendering/renderablesatellites.h
@@ -1,8 +1,8 @@
-/****************************************************************************************
+/*****************************************************************************************
  *                                                                                       *
  * OpenSpace                                                                             *
  *                                                                                       *
- * Copyright (c) 2014-2018                                                               *
+ * Copyright (c) 2014-2020                                                               *
  *                                                                                       *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this  *
  * software and associated documentation files (the "Software"), to deal in the Software *
