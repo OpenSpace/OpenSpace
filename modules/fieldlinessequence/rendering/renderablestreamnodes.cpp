cb204cd3fd7c54f6b4ddaf18f0f7a7d14e8962b0
Christian Adamsson <Christian.liu171@gmail.com>
2020-07-13 11:23:32 +0200

    Fixed bug with binary sync files

--- modules/fieldlinessequence/rendering/renderablestreamnodes.cpp
+++ modules/fieldlinessequence/rendering/renderablestreamnodes.cpp
@@ -498,11 +498,12 @@ void RenderableStreamNodes::initializeGL() {
 }
 
 void RenderableStreamNodes::loadNodeData() {
-
+    createStreamnumberVector();
     if (shouldreadBinariesDirectly) {
         bool success = loadBinaryfilesDirectly("");
         if(success) return;
     }
+    LDEBUG("Vi reached outside of loadbinariesdirectly");
     std::string _file = "StreamnodesCachePosition";
     std::string _file2 = "StreamnodesCacheColor";
     std::string _file3 = "StreamnodesCacheRadius";
@@ -824,6 +825,7 @@ bool RenderableStreamNodes::loadBinaryfilesDirectly(const std::string& energybin
     std::string _file = _binarySourceFilePath  + "\\StreamnodesCachePosition" + energybin;
     std::string _file2 = _binarySourceFilePath + "\\StreamnodesCacheColor" + energybin;
     std::string _file3 = _binarySourceFilePath + "\\StreamnodesCacheRadius" + energybin;
+    /*
     std::string cachedFile = FileSys.cacheManager()->cachedFilename(
         _file,
         ghoul::filesystem::CacheManager::Persistent::Yes
@@ -836,10 +838,16 @@ bool RenderableStreamNodes::loadBinaryfilesDirectly(const std::string& energybin
         _file3,
         ghoul::filesystem::CacheManager::Persistent::Yes
     );
+   
     std::ifstream fileStream(cachedFile, std::ifstream::binary);
     std::ifstream fileStream2(cachedFile2, std::ifstream::binary);
     std::ifstream fileStream3(cachedFile3, std::ifstream::binary);
-
+    */
+     
+    std::ifstream fileStream(_file, std::ifstream::binary);
+    std::ifstream fileStream2(_file2, std::ifstream::binary);
+    std::ifstream fileStream3(_file3, std::ifstream::binary);
+    
     if (fileStream.good()) {
         int8_t version = 0;
         fileStream.read(reinterpret_cast<char*>(&version), sizeof(int8_t));
@@ -1188,7 +1196,7 @@ void RenderableStreamNodes::render(const RenderData& data, RendererTasks&) {
         _shaderProgram->setUniform("modelViewProjection",
             data.camera.sgctInternal.projectionMatrix() * glm::mat4(modelViewMat));
 
-
+    // this is taken by Jons data, each point multiplicated by Au to Meter.
     glm::vec3 earthPos = glm::vec3(94499869340, -115427843118, 11212075887.3);
     //SceneGraphNode* earthNode = sceneGraphNode("Earth");
     
