e2da38747388878927b4cff71785b050cb2e96cf
Micah Acinapura <micahnyc@gmail.com>
2020-12-08 10:19:07 -0500

    added spice frame info functions to spice manager (#1427)
    * Added spice frame info functions to spice manager

--- src/util/spicemanager.cpp
+++ src/util/spicemanager.cpp
@@ -342,6 +342,23 @@ bool SpiceManager::hasSpkCoverage(const std::string& target, double et) const {
     return false;
 }
 
+std::vector<std::pair<double, double>> SpiceManager::spkCoverage(
+                                                          const std::string& target) const
+{
+    ghoul_assert(!target.empty(), "Empty target");
+
+    const int id = naifId(target);
+    const auto it = _spkIntervals.find(id);
+    if (it != _spkIntervals.end()) {
+        return it->second;
+    }
+    else {
+        std::vector<std::pair<double, double>> emptyList;
+        return emptyList;
+    }
+}
+
+
 bool SpiceManager::hasCkCoverage(const std::string& frame, double et) const {
     ghoul_assert(!frame.empty(), "Empty target");
 
@@ -356,6 +373,71 @@ bool SpiceManager::hasCkCoverage(const std::string& frame, double et) const {
         }
     }
     return false;
+}
+
+std::vector<std::pair<double, double>> SpiceManager::ckCoverage(
+                                                          const std::string& target) const
+{
+    ghoul_assert(!target.empty(), "Empty target");
+
+    int id = naifId(target);
+    const auto it = _ckIntervals.find(id);
+    if (it != _ckIntervals.end()) {
+        return it->second;
+    }
+    else {
+        id *= 1000;
+        const auto it = _ckIntervals.find(id);
+        if (it != _ckIntervals.end()) {
+            return it->second;
+        }
+        else {
+            std::vector<std::pair<double, double>> emptyList;
+            return emptyList;
+        }
+    }
+}
+
+std::vector<std::pair<int, std::string>> SpiceManager::spiceBodies(
+                                                                 bool builtInFrames) const
+{
+    std::vector<std::pair<int, std::string>> bodies;
+
+    constexpr const int Frnmln = 33;
+    constexpr const int Lnsize = 81;
+    SPICEINT_CELL(idset, 8192);
+
+    SpiceChar frname[Frnmln];
+    SpiceChar outlin[Lnsize];
+
+    for (SpiceInt i = 1; i <= 6; i++) {
+        if (i < 6) {
+            if (builtInFrames) {
+                bltfrm_c(i, &idset);
+            }
+            else {
+                kplfrm_c(i, &idset);
+            }
+        }
+        else {
+            if (builtInFrames) {
+                bltfrm_c(SPICE_FRMTYP_ALL, &idset);
+            }
+            else {
+                kplfrm_c(SPICE_FRMTYP_ALL, &idset);
+            }
+        }
+
+        for (SpiceInt j = 0; j < card_c(&idset); j++) {
+            frmnam_c(
+                (reinterpret_cast<SpiceInt*>(idset.data))[j],
+                Frnmln,
+                frname
+            );
+            bodies.push_back(std::make_pair(((long)((SpiceInt*)idset.data)[j]), frname));
+        }
+    }
+    return bodies;
 }
 
 bool SpiceManager::hasValue(int naifId, const std::string& item) const {
@@ -1250,7 +1332,30 @@ scripting::LuaLibrary SpiceManager::luaLibrary() {
                 "{string, number}",
                 "Unloads the provided SPICE kernel. The name can contain path tokens, "
                 "which are automatically resolved"
+            },
+            {
+                "getSpkCoverage",
+                &luascriptfunctions::spkCoverage,
+                {},
+                "{string [, printValues]}",
+                "Returns a list of SPK coverage intervals for the target."
+            },
+            {
+                "getCkCoverage",
+                & luascriptfunctions::ckCoverage,
+                {},
+                "{string [, printValues]}",
+                "Returns a list of CK coverage intervals for the target."
+            },
+            {
+                "getSpiceBodies",
+                &luascriptfunctions::spiceBodies,
+                {},
+                "{builtInFrames [, printValues]}",
+                "Returns a list of Spice Bodies loaded into the system. Returns SPICE built in frames if builtInFrames"
+                "Returns User loaded frames if !builtInFrames"
             }
+
         }
     };
 }
