61b7c10d3afa7a0e208c42241d5968a6c36d4ea0
Micah Acinapura <micahnyc@gmail.com>
2020-12-09 17:43:21 -0500

    removing fxaa disable on macos

--- src/rendering/renderengine.cpp
+++ src/rendering/renderengine.cpp
@@ -301,12 +301,6 @@ RenderEngine::RenderEngine()
     addProperty(_showVersionInfo);
     addProperty(_showCameraInfo);
 
-    // @TODO (maci 2019-08-23) disabling FXAA on
-    // MacOS for now until we have fix or MSAA option.
-#ifdef __APPLE__
-    _enableFXAA = false;
-#endif
-
     _enableFXAA.onChange([this]() {
         if (_renderer) {
             _renderer->enableFXAA(_enableFXAA);
