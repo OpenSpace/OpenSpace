3fb44d63cf7167a89ca25cd748c5496bc7dba15d
Emma Broman <emma.broman@liu.se>
2021-01-11 10:41:13 +0100

    Bring back fading of sphere with only fade out threshold specified
    Without this, the milkyway sphere does not fade out at all
    
    # Conflicts:
    #	modules/base/rendering/renderablesphere.cpp

--- modules/base/rendering/renderablesphere.cpp
+++ modules/base/rendering/renderablesphere.cpp
@@ -282,8 +282,8 @@ RenderableSphere::RenderableSphere(const ghoul::Dictionary& dictionary)
         addProperty(_fadeInThreshold);
     }
 
-    if (dictionary.hasKey(FadeOutThresholdInfo.identifier) ||
-        dictionary.hasKey(FadeInThresholdInfo.identifier)) {
+    if (dictionary.hasKey(FadeInThresholdInfo.identifier) ||
+        dictionary.hasKey(FadeOutThresholdInfo.identifier)) {
         _disableFadeInDistance.set(false);
         addProperty(_disableFadeInDistance);
     }
