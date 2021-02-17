6837bad5b0769650657f8c30ed670b8b61b29f00
Alexander Bock <alexander.bock@liu.se>
2021-02-17 16:18:08 +0100

    Tiny cleanups

--- src/engine/moduleengine.cpp
+++ src/engine/moduleengine.cpp
@@ -24,6 +24,7 @@
 
 #include <openspace/engine/moduleengine.h>
 
+#include <openspace/documentation/documentation.h>
 #include <openspace/moduleregistration.h>
 #include <openspace/scripting/lualibrary.h>
 #include <openspace/util/openspacemodule.h>
