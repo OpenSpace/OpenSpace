7d58cf40c2ea56c6b49bb0473f5c78f4dd36d4a2
Ester Lindgren <estli612@student.liu.se>
2021-02-23 14:07:34 +0100

    Add module for skybrowser

---
+++ modules/skybrowser/skybrowsermodule.cpp
@@ -0,0 +1,103 @@
+/*****************************************************************************************
+ *                                                                                       *
+ * OpenSpace                                                                             *
+ *                                                                                       *
+ * Copyright (c) 2014-2021                                                               *
+ *                                                                                       *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this  *
+ * software and associated documentation files (the "Software"), to deal in the Software *
+ * without restriction, including without limitation the rights to use, copy, modify,    *
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to    *
+ * permit persons to whom the Software is furnished to do so, subject to the following   *
+ * conditions:                                                                           *
+ *                                                                                       *
+ * The above copyright notice and this permission notice shall be included in all copies *
+ * or substantial portions of the Software.                                              *
+ *                                                                                       *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,   *
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A         *
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT    *
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF  *
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE  *
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                         *
+ ****************************************************************************************/
+
+#include <modules/skybrowser/skybrowsermodule.h>
+
+
+#include <openspace/engine/globals.h>
+#include <openspace/engine/globalscallbacks.h>
+#include <openspace/interaction/navigationhandler.h>
+#include <openspace/rendering/renderengine.h>
+#include <openspace/scene/scenegraphnode.h>
+#include <openspace/scene/scene.h>
+#include <openspace/util/factorymanager.h>
+#include <thread>
+#include <chrono>
+
+
+#include "skybrowsermodule_lua.inl"
+
+
+namespace {
+    constexpr const openspace::properties::Property::PropertyInfo TestInfo = 
+    {
+        "TestInfo",
+        "Test Info",
+        "tjobidabidobidabidopp plopp"
+    };
+
+    struct [[codegen::Dictionary(SkybrowserModule)]] Parameters {
+
+        // [[codegen::verbatim(TestInfo.description)]]
+        std::optional<std::string> testString;
+    };
+    
+    #include "skybrowsermodule_codegen.cpp"
+    
+    
+} // namespace
+
+namespace openspace {
+
+SkybrowserModule::SkybrowserModule()
+    : OpenSpaceModule(Name)
+    , _testProperty(TestInfo)
+{
+    addProperty(_testProperty);
+}
+
+scripting::LuaLibrary SkybrowserModule::luaLibrary() const {
+    scripting::LuaLibrary res;
+    res.name = "skybrowser";
+    res.functions = {
+        {
+            "test",
+            &skybrowser::luascriptfunctions::testFunction,
+            {},
+            "string or list of strings",
+            "Add one or multiple exoplanet systems to the scene, as specified by the "
+            "input. An input string should be the name of the system host star"
+        }
+    };
+
+    return res;
+}
+
+void SkybrowserModule::internalInitialize(const ghoul::Dictionary& dict) {
+    const Parameters p = codegen::bake<Parameters>(dict);
+    _testProperty = p.testString.value_or(_testProperty);
+
+    auto fBrowser = FactoryManager::ref().factory<ScreenSpaceBrowser>();
+    ghoul_assert(fBrowser, "No browser factory existed :'-(");
+    fBrowser->registerClass<ScreenSpaceBrowser>("ScreenSpaceBrowser");
+}
+/*
+std::vector<documentation::Documentation> SkybrowserModule::documentations() const {
+    return {
+        ExoplanetsDataPreparationTask::documentation(),
+        RenderableOrbitDisc::Documentation()
+    };
+}
+*/
+} // namespace openspace
---
+++ modules/skybrowser/skybrowsermodule_lua.inl
@@ -0,0 +1,78 @@
+#include <openspace/util/openspacemodule.h>
+
+
+#include <openspace/documentation/documentation.h>
+#include <modules/skybrowser/skybrowsermodule.h>
+
+#include <openspace/scripting/scriptengine.h>
+#include <ghoul/misc/dictionaryluaformatter.h>
+
+#include <ghoul/filesystem/filesystem.h>
+#include <ghoul/fmt.h>
+#include <ghoul/glm.h>
+#include <ghoul/logging/logmanager.h>
+#include <ghoul/misc/assert.h>
+#include <fstream>
+#include <sstream>
+
+
+namespace {
+    constexpr const char _loggerCat[] = "SkybrowserModule";
+} // namespace
+
+
+namespace openspace::skybrowser::luascriptfunctions {
+    
+    bool testFunction() {
+        LINFOC(_loggerCat, "yabadadooo");
+        return true;
+    }
+
+
+    int testFunction(lua_State* L) {
+        ghoul::lua::checkArgumentsAndThrow(L, 0, "lua::testFunction");
+
+
+        LINFOC(_loggerCat, "hoho");
+        testFunction();
+
+        //std::string _url = "https://wallpaperaccess.com/full/3010132.jpg";
+        // 'https://cdn.wallpapersafari.com/6/92/0nbCPw.jpg'
+        /*
+        // get url from user
+        const std::string _url = ghoul::lua::value<std::string>(L, 1);
+       
+        using namespace std::string_literals;
+
+        std::string identifier = "ImageTest";
+        std::string guiname = "Test";
+        double size = 1.E11;
+
+        // create renderable renderableplaneimageonline
+        ghoul::Dictionary renderable; 
+        renderable.setValue("Type", "RenderablePlaneImageOnline"s);
+        renderable.setValue("URL", _url);
+        renderable.setValue("Origin", "Center"s);
+        renderable.setValue("Size", size);
+
+        ghoul::Dictionary gui;
+        gui.setValue("Name", guiname);
+        gui.setValue("Path", "/Software Integration"s);
+
+        ghoul::Dictionary node;
+        node.setValue("Identifier", identifier);
+        node.setValue("Renderable", renderable);
+        node.setValue("GUI", gui);
+
+
+        openspace::global::scriptEngine->queueScript(
+            "openspace.addSceneGraphNode(" + ghoul::formatLua(node) + ")",
+            scripting::ScriptEngine::RemoteScripting::Yes
+        );
+        */
+
+        return 1;
+    }
+    
+}
+
