0d82e1848f671c826202f72cd41d5560e4910a67
Alexander Bock <mail@alexanderbock.eu>
2021-03-23 15:18:55 +0100

    Feature/configuration codegen (#1544)
    Convert configuration document over to codegen

--- apps/OpenSpace/main.cpp
+++ apps/OpenSpace/main.cpp
@@ -1105,22 +1105,17 @@ int main(int argc, char* argv[]) {
         }
         LINFO(fmt::format("Configuration Path: '{}'", configurationFilePath));
 
+        // Register the base path as the directory where the configuration file lives
+        std::string base = ghoul::filesystem::File(configurationFilePath).directoryName();
+        constexpr const char* BasePathToken = "${BASE}";
+        FileSys.registerPathToken(BasePathToken, base);
+
         // Loading configuration from disk
         LDEBUG("Loading configuration from disk");
         *global::configuration = configuration::loadConfigurationFromFile(
-            configurationFilePath
+            configurationFilePath,
+            commandlineArguments.configurationOverride
         );
-        // If the user requested a commandline-based configuration script that should
-        // overwrite some of the values, this is the time to do it
-        if (!commandlineArguments.configurationOverride.empty()) {
-            LDEBUG("Executing Lua script passed through the commandline:");
-            LDEBUG(commandlineArguments.configurationOverride);
-            ghoul::lua::runScript(
-                global::configuration->state,
-                commandlineArguments.configurationOverride
-            );
-            parseLuaState(*global::configuration);
-        }
 
         // Determining SGCT configuration file
         LDEBUG("SGCT Configuration file: " + global::configuration->windowConfiguration);
