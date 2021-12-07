e14121741a702a17043ff46ad7842ec0ec7748e2
Malin E <malin.ejdbo@gmail.com>
2021-12-07 09:05:16 +0100

    Dont spam warning for too many axes or buttons on joystick

--- apps/OpenSpace/main.cpp
+++ apps/OpenSpace/main.cpp
@@ -369,29 +369,32 @@ void mainPreSyncFunc() {
 
             std::fill(state.axes.begin(), state.axes.end(), 0.f);
             std::fill(state.buttons.begin(), state.buttons.end(), JoystickAction::Idle);
+
+            // Check axes and buttons
+            glfwGetJoystickAxes(i, &state.nAxes);
+            if (state.nAxes > JoystickInputState::MaxAxes) {
+                LWARNING(fmt::format(
+                    "Joystick/Gamepad {} has {} axes, but only {} axes are supported. "
+                    "All excess axes are ignored",
+                    state.name, state.nAxes, JoystickInputState::MaxAxes
+                ));
+                state.nAxes = JoystickInputState::MaxAxes;
+            }
+            glfwGetJoystickButtons(i, &state.nButtons);
+            if (state.nButtons > JoystickInputState::MaxButtons) {
+                LWARNING(fmt::format(
+                    "Joystick/Gamepad {} has {} buttons, but only {} buttons are "
+                    "supported. All excess buttons are ignored",
+                    state.name, state.nButtons, JoystickInputState::MaxButtons
+                ));
+                state.nButtons = JoystickInputState::MaxButtons;
+            }
         }
 
         const float* axes = glfwGetJoystickAxes(i, &state.nAxes);
-        if (state.nAxes > JoystickInputState::MaxAxes) {
-            LWARNING(fmt::format(
-                "Joystick/Gamepad {} has {} axes, but only {} axes are supported. "
-                "All excess axes are ignored",
-                state.name, state.nAxes, JoystickInputState::MaxAxes
-            ));
-            state.nAxes = JoystickInputState::MaxAxes;
-        }
         std::memcpy(state.axes.data(), axes, state.nAxes * sizeof(float));
 
         const unsigned char* buttons = glfwGetJoystickButtons(i, &state.nButtons);
-
-        if (state.nButtons > JoystickInputState::MaxButtons) {
-            LWARNING(fmt::format(
-                "Joystick/Gamepad {} has {} buttons, but only {} buttons are "
-                "supported. All excess buttons are ignored",
-                state.name, state.nButtons, JoystickInputState::MaxButtons
-            ));
-            state.nButtons = JoystickInputState::MaxButtons;
-        }
 
         for (int j = 0; j < state.nButtons; ++j) {
             const bool currentlyPressed = buttons[j] == GLFW_PRESS;
