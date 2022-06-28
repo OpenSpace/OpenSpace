3038280f595361b90d3d7f7fbeff1cf49e27316a
mathis <mathis.brossier@universite-paris-saclay.fr>
2022-06-28 10:57:45 +0200

    Fix filename when downloading multiple files in urlsunc

--- modules/sync/syncs/urlsynchronization.cpp
+++ modules/sync/syncs/urlsynchronization.cpp
@@ -147,7 +147,7 @@ void UrlSynchronization::start() {
         std::vector<std::unique_ptr<HttpFileDownload>> downloads;
 
         for (const std::string& url : _urls) {
-            if (_filename.empty()) {
+            if (_filename.empty() || _urls.size() > 1) {
                 std::string name = std::filesystem::path(url).filename().string();
 
                 // We can not create filenames with question marks
