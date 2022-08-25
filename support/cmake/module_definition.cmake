640815a6f6800e9254cc0e2c95654d546ef6e8ca
Alexander Bock <alexander.bock@liu.se>
2022-08-25 09:29:48 +0200

    Bring the compile time below 30 minutes

--- support/cmake/module_definition.cmake
+++ support/cmake/module_definition.cmake
@@ -108,9 +108,6 @@ endfunction ()
 function (handle_module_dependencies target_name module_name)
   # We always want to link against Ghoul and the core library
   target_link_libraries(${library_name} PRIVATE Ghoul openspace-core)
-  # We currently can't reuse the precompiled header because that one has the Kameleon
-  # definition stuck into it
-  #target_precompile_headers(${library_name} REUSE_FROM openspace-core)
   target_precompile_headers(${library_name} PRIVATE
     [["ghoul/fmt.h"]]
     [["ghoul/glm.h"]]
