12bb47c71e2cd8a3ce4b8c1697604ed1f2dffe3a
Alexander Bock <alexander.bock@liu.se>
2022-11-07 15:45:54 +0100

    Recalculate chunk bounding boxes when the settings of a heightlayer change (closes #2096)
    * Correctly calculate Offset when negative numbers are involved
    * Adds tile index layer for the Moon
    * Remove empty spaces

--- modules/globebrowsing/shaders/localrenderer_vs.glsl
+++ modules/globebrowsing/shaders/localrenderer_vs.glsl
@@ -90,7 +90,7 @@ vec3 getLevelWeights(float distToVertexOnEllipsoid) {
 void main() {
   // Position in cameraspace
   vec3 p = bilinearInterpolation(in_uv);
-  
+
   // Calculate desired level based on distance to the vertex on the ellipsoid
   // Before any heightmapping is done
   float distToVertexOnEllipsoid = length(p + patchNormalCameraSpace * chunkMinHeight);
@@ -100,7 +100,7 @@ void main() {
 
   // Get the height value and apply skirts
   float height = getTileHeightScaled(in_uv, levelWeights) - getTileVertexSkirtLength() * 100.0;
-  
+
   // Translate the point along normal
   p += patchNormalCameraSpace * height;
 
