e69ec8ac4d9606202a8d055d81e9a4495608a03d
Adam Rohdin <adam.rohdin@liu.se>
2023-02-13 10:14:08 +0100

    Neater solution than cb45f96

--- src/interaction/keybindingmanager.cpp
+++ src/interaction/keybindingmanager.cpp
@@ -58,10 +58,9 @@ void KeybindingManager::keyboardCallback(Key key, KeyModifier modifier, KeyActio
             if (!global::actionManager->hasAction(it->second)) {
                 // Silently ignoring the unknown action as the user might have intended to
                 // bind a key to multiple actions, only one of which could be defined
+                continue;
             }
-            else {
-                global::actionManager->triggerAction(it->second, ghoul::Dictionary());
-            }
+            global::actionManager->triggerAction(it->second, ghoul::Dictionary());
         }
     }
 }
