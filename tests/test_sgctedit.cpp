e287106d68c15eb78ec1b379482b5b5452df2e01
Gene Payne <payne.gene@gmail.com>
2023-03-30 15:23:41 -0600

    Finished tests for sgcteditor validation

--- tests/test_sgctedit.cpp
+++ tests/test_sgctedit.cpp
@@ -24,9 +24,11 @@
 
 #include "catch2/catch.hpp"
 
+#include <ghoul/fmt.h>
+#include <nlohmann/json.hpp>
+#include <nlohmann/json-schema.hpp>
 #include <sgct/readconfig.h>
 #include <openspace/engine/configuration.h>
-#include <ghoul/fmt.h>
 #include <filesystem>
 #include <fstream>
 
@@ -118,4 +120,495 @@ R"({
   "version": 1
 })";
     CHECK_NOTHROW(attemptValidation(config));
+}
+
+TEST_CASE("SgctEdit: addedTrailingBracket", "[sgctedit]") {
+    const std::string config =
+R"({
+  "generator": {
+    "major": 0,
+    "minor": 1,
+    "name": "SgctWindowConfig"
+  },
+  "masteraddress": "localhost",
+  "nodes": [
+    {
+      "address": "localhost",
+      "port": 20401,
+      "windows": [
+        {
+          "border": true,
+          "id": 0,
+          "monitor": 0,
+          "name": "ffss",
+          "pos": {
+            "x": 112,
+            "y": 77
+          },
+          "size": {
+            "x": 1280,
+            "y": 720
+          },
+          "viewports": [
+            {
+              "pos": {
+                "x": 0.0,
+                "y": 0.0
+              },
+              "projection": {
+                "heightoffset": 0.0,
+                "quality": "1024",
+                "type": "CylindricalProjection"
+              },
+              "size": {
+                "x": 1.0,
+                "y": 1.0
+              },
+              "tracked": true
+            }
+          ]
+        }
+      ]
+    }
+  ],
+  "scene": {
+    "orientation": {
+      "w": 0.0,
+      "x": 0.0,
+      "y": 0.0,
+      "z": 0.0
+    }
+  },
+  "users": [
+    {
+      "eyeseparation": 0.06499999761581421,
+      "pos": {
+        "x": 0.0,
+        "y": 0.0,
+        "z": 4.0
+      }
+    }
+  ],
+  "version": 1
+}})";
+    CHECK_THROWS_MATCHES(
+        attemptValidation(config),
+        nlohmann::json::parse_error,
+        Catch::Matchers::Message(
+            "[json.exception.parse_error.101] parse error at line 67, column 2: "
+            "syntax error while parsing value - unexpected '}'; expected "
+            "end of input"
+        )
+    );
+}
+
+TEST_CASE("SgctEdit: missingMasterAddress", "[sgctedit]") {
+    const std::string config =
+R"({
+  "generator": {
+    "major": 1,
+    "minor": 1,
+    "name": "SgctWindowConfig"
+  },
+  "nodes": [
+    {
+      "address": "localhost",
+      "port": 20401,
+      "windows": [
+        {
+          "border": true,
+          "id": 0,
+          "monitor": 1,
+          "name": "name",
+          "pos": {
+            "x": 112,
+            "y": 77
+          },
+          "size": {
+            "x": 1280,
+            "y": 720
+          },
+          "viewports": [
+            {
+              "pos": {
+                "x": 0.0,
+                "y": 0.0
+              },
+              "projection": {
+                "heightoffset": 0.0,
+                "quality": "1024",
+                "type": "CylindricalProjection"
+              },
+              "size": {
+                "x": 1.0,
+                "y": 1.0
+              },
+              "tracked": true
+            }
+          ]
+        }
+      ]
+    }
+  ],
+  "scene": {
+    "orientation": {
+      "w": 0.0,
+      "x": 0.0,
+      "y": 0.0,
+      "z": 0.0
+    }
+  },
+  "users": [
+    {
+      "eyeseparation": 0.06499999761581421,
+      "pos": {
+        "x": 0.0,
+        "y": 0.0,
+        "z": 4.0
+      }
+    }
+  ],
+  "version": 1
+})";
+    CHECK_THROWS_MATCHES(
+        attemptValidation(config),
+        std::exception,
+        Catch::Matchers::Message(
+            "At  of {\"generator\":{\"major\":1,\"minor\":1,\"name\":"
+            "\"SgctWindowConfig\"},\"nodes\":[{\"address\":\"localhost\",\"port\":"
+            "20401,\"windows\":[{\"border\":true,\"id\":0,\"monitor\":1,\"name\":"
+            "\"name\",\"pos\":{\"x\":112,\"y\":77},\"size\":{\"x\":1280,\"y\":720},"
+            "\"viewports\":[{\"pos\":{\"x\":0.0,\"y\":0.0},\"projection\":"
+            "{\"heightoffset\":0.0,\"quality\":\"1024\",\"type\":"
+            "\"CylindricalProjection\"},\"size\":{\"x\":1.0,\"y\":1.0},\"tracked\":"
+            "true}]}]}],\"scene\":{\"orientation\":{\"w\":0.0,\"x\":0.0,\"y\":0.0,"
+            "\"z\":0.0}},\"users\":[{\"eyeseparation\":0.06499999761581421,\"pos\":"
+            "{\"x\":0.0,\"y\":0.0,\"z\":4.0}}],\"version\":1} - required property "
+            "'masteraddress' not found in object\n"
+        )
+    );
+}
+
+TEST_CASE("SgctEdit: missingPos", "[sgctedit]") {
+    const std::string config =
+R"({
+  "generator": {
+    "major": 1,
+    "minor": 1,
+    "name": "SgctWindowConfig"
+  },
+  "masteraddress": "localhost",
+  "nodes": [
+    {
+      "address": "localhost",
+      "port": 20401,
+      "windows": [
+        {
+          "border": true,
+          "id": 0,
+          "monitor": 0,
+          "name": "name",
+          "pos": {
+            "x": 112,
+            "y": 77
+          },
+          "size": {
+            "x": 1280,
+            "y": 720
+          },
+          "viewports": [
+            {
+              "pos": {
+                "x": 0.0,
+                "y": 0.0
+              },
+              "projection": {
+                "heightoffset": 0.0,
+                "quality": "1024",
+                "type": "CylindricalProjection"
+              },
+              "size": {
+                "x": 1.0,
+                "y": 1.0
+              },
+              "tracked": true
+            }
+          ]
+        }
+      ]
+    }
+  ],
+  "scene": {
+    "orientation": {
+      "w": 0.0,
+      "x": 0.0,
+      "y": 0.0,
+      "z": 0.0
+    }
+  },
+  "users": [
+    {
+      "eyeseparation": 0.06499999761581421
+    }
+  ],
+  "version": 1
+})";
+    CHECK_THROWS_MATCHES(
+        attemptValidation(config),
+        std::exception,
+        Catch::Matchers::Message(
+            "At /users/0 of {\"eyeseparation\":0.06499999761581421} - required "
+            "property 'pos' not found in object\n"
+        )
+    );
+}
+
+TEST_CASE("SgctEdit: missingGenerator", "[sgctedit]") {
+    const std::string config =
+R"({
+  "masteraddress": "localhost",
+  "nodes": [
+    {
+      "address": "localhost",
+      "port": 20401,
+      "windows": [
+        {
+          "border": true,
+          "id": 0,
+          "monitor": 0,
+          "name": "name",
+          "pos": {
+            "x": 112,
+            "y": 77
+          },
+          "size": {
+            "x": 1280,
+            "y": 720
+          },
+          "viewports": [
+            {
+              "pos": {
+                "x": 0.0,
+                "y": 0.0
+              },
+              "projection": {
+                "heightoffset": 0.0,
+                "quality": "1024",
+                "type": "CylindricalProjection"
+              },
+              "size": {
+                "x": 1.0,
+                "y": 1.0
+              },
+              "tracked": true
+            }
+          ]
+        }
+      ]
+    }
+  ],
+  "scene": {
+    "orientation": {
+      "w": 0.0,
+      "x": 0.0,
+      "y": 0.0,
+      "z": 0.0
+    }
+  },
+  "users": [
+    {
+      "eyeseparation": 0.06499999761581421,
+      "pos": {
+        "x": 0.0,
+        "y": 0.0,
+        "z": 4.0
+      }
+    }
+  ],
+  "version": 1
+})";
+    CHECK_THROWS_MATCHES(
+        attemptValidation(config),
+        std::exception,
+        Catch::Matchers::Message(
+            "At  of {\"masteraddress\":\"localhost\",\"nodes\":[{\"address\":"
+            "\"localhost\",\"port\":20401,\"windows\":[{\"border\":true,\"id\":"
+            "0,\"monitor\":0,\"name\":\"name\",\"pos\":{\"x\":112,\"y\":77},\"size\":"
+            "{\"x\":1280,\"y\":720},\"viewports\":[{\"pos\":{\"x\":0.0,\"y\":0.0},"
+            "\"projection\":{\"heightoffset\":0.0,\"quality\":\"1024\",\"type\":"
+            "\"CylindricalProjection\"},\"size\":{\"x\":1.0,\"y\":1.0},\"tracked\":"
+            "true}]}]}],\"scene\":{\"orientation\":{\"w\":0.0,\"x\":0.0,\"y\":0.0,\"z\":"
+            "0.0}},\"users\":[{\"eyeseparation\":0.06499999761581421,\"pos\":{\"x\":"
+            "0.0,\"y\":0.0,\"z\":4.0}}],\"version\":1} - required property 'generator' "
+            "not found in object\n"
+        )
+    );
+}
+
+TEST_CASE("SgctEdit: minimumVersion", "[sgctedit]") {
+    const sgct::config::GeneratorVersion minVersion { "SgctWindowConfig", 1, 1 };
+    const std::string inputCfg = "../tests/sgctedit/fails_minimum_version.json";
+    sgct::config::GeneratorVersion ver = sgct::readConfigGenerator(inputCfg);
+    CHECK_FALSE(ver.versionCheck(minVersion));
+}
+
+TEST_CASE("SgctEdit: invalidZvalue", "[sgctedit]") {
+    const std::string config =
+R"({
+  "generator": {
+    "major": 1,
+    "minor": 1,
+    "name": "SgctWindowConfig"
+  },
+  "masteraddress": "localhost",
+  "nodes": [
+    {
+      "address": "localhost",
+      "port": 20401,
+      "windows": [
+        {
+          "border": true,
+          "id": 0,
+          "monitor": 1,
+          "name": "ffss",
+          "pos": {
+            "x": 112,
+            "y": 77
+          },
+          "size": {
+            "x": 1280,
+            "y": 720,
+	    "z": s
+          },
+          "viewports": [
+            {
+              "pos": {
+                "x": 0.0,
+                "y": 0.0
+              },
+              "projection": {
+                "heightoffset": 0.0,
+                "quality": "1024",
+                "type": "CylindricalProjection"
+              },
+              "size": {
+                "x": 1.0,
+                "y": 1.0
+              },
+              "tracked": true
+            }
+          ]
+        }
+      ]
+    }
+  ],
+  "scene": {
+    "orientation": {
+      "w": 0.0,
+      "x": 0.0,
+      "y": 0.0,
+      "z": 0.0
+    }
+  },
+  "users": [
+    {
+      "eyeseparation": 0.06499999761581421,
+      "pos": {
+        "x": 0.0,
+        "y": 0.0,
+        "z": 4.0
+      }
+    }
+  ],
+  "version": 1
+})";
+    CHECK_THROWS_MATCHES(
+        attemptValidation(config),
+        std::exception,
+        Catch::Matchers::Message(
+            "[json.exception.parse_error.101] parse error at line 25, column 11: "
+            "syntax error while parsing value - invalid literal; last read: '\"z\": s'"
+        )
+    );
+}
+
+TEST_CASE("SgctEdit: unwelcomeValue", "[sgctedit]") {
+    const std::string config =
+R"({
+  "generator": {
+    "major": 1,
+    "minor": 1,
+    "name": "SgctWindowConfig"
+  },
+  "masteraddress": "localhost",
+  "nodes": [
+    {
+      "address": "localhost",
+      "port": 20401,
+      "windows": [
+        {
+          "border": true,
+          "id": 0,
+          "monitor": 0,
+          "name": "ffss",
+          "pos": {
+            "x": 112,
+            "y": 77
+          },
+          "size": {
+            "x": 1280,
+            "y": 720
+          },
+          "viewports": [
+            {
+              "pos": {
+                "x": 0.0,
+                "y": 0.0
+              },
+              "projection": {
+                "heightoffset": 0.0,
+                "quality": "1024",
+                "type": "CylindricalProjection"
+              },
+              "size": {
+                "x": 1.0,
+                "y": 1.0
+              },
+              "tracked": true
+            }
+          ]
+        }
+      ]
+    }
+  ],
+  "scene": {
+    "orientation": {
+      "w": 0.0,
+      "x": 0.0,
+      "y": 0.0,
+      "z": 0.0
+    }
+  },
+  "users": [
+    {
+      "extra": "???",
+      "eyeseparation": 0.6,
+      "pos": {
+        "x": 0.0,
+        "y": 0.0,
+        "z": 4.0
+      }
+    }
+  ],
+  "version": 1
+})";
+    CHECK_THROWS_MATCHES(
+        attemptValidation(config),
+        std::exception,
+        Catch::Matchers::Message(
+            "At /users/0 of {\"extra\":\"???\",\"eyeseparation\":0.6,\"pos\":"
+            "{\"x\":0.0,\"y\":0.0,\"z\":4.0}} - validation failed for additional "
+            "property 'extra': instance invalid as per false-schema\n"
+        )
+    );
 }
