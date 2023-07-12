a244c5cacf71afca8207e1f91517eeb7912769e8
Alexander Bock <alexander.bock@liu.se>
2023-07-12 10:49:50 +0200

    Add the ability to specify the angular size of the Sun in the atmosphere

--- modules/atmosphere/rendering/atmospheredeferredcaster.h
+++ modules/atmosphere/rendering/atmospheredeferredcaster.h
@@ -88,7 +88,7 @@ public:
         float mieHeightScale, float miePhaseConstant, float sunRadiance,
         glm::vec3 rayScatteringCoefficients, glm::vec3 ozoneExtinctionCoefficients,
         glm::vec3 mieScatteringCoefficients, glm::vec3 mieExtinctionCoefficients,
-        bool sunFollowing);
+        bool sunFollowing, float sunAngularSize);
 
     void setHardShadows(bool enabled);
 
@@ -119,7 +119,7 @@ private:
         betaOzoneExtinction, SAMPLES_R, SAMPLES_MU, SAMPLES_MU_S, SAMPLES_NU,
         inverseModelTransformMatrix, modelTransformMatrix, projectionToModelTransform,
         viewToWorldMatrix, camPosObj, sunDirectionObj, hardShadows, transmittanceTexture,
-        irradianceTexture, inscatterTexture) _uniformCache;
+        irradianceTexture, inscatterTexture, sunAngularSize) _uniformCache;
 
     ghoul::opengl::TextureUnit _transmittanceTableTextureUnit;
     ghoul::opengl::TextureUnit _irradianceTableTextureUnit;
@@ -141,6 +141,7 @@ private:
     float _mieHeightScale = 0.f;
     float _miePhaseConstant = 0.f;
     float _sunRadianceIntensity = 5.f;
+    float _sunAngularSize = 0.3f;
 
     glm::vec3 _rayleighScatteringCoeff = glm::vec3(0.f);
     glm::vec3 _ozoneExtinctionCoeff = glm::vec3(0.f);
