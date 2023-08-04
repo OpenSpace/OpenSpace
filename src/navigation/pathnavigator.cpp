f78f42fbd5aa348f56e5a7e41f3e94839a86e720
Emma Broman <emma.broman@liu.se>
2023-08-04 08:31:49 +0200

    Fix miguiding RotationSpeedFactor documentation, and increase default value (closes #2838)

--- src/navigation/pathnavigator.cpp
+++ src/navigation/pathnavigator.cpp
@@ -101,7 +101,7 @@ namespace {
         "Rotation Speed Factor (Linear Path)",
         "Affects how fast the camera rotates to the target rotation during a linear "
         "path. A value of 1 means that the camera will rotate 90 degrees in about 5 "
-        "seconds. A value of 2 means twice that fast, and so on",
+        "seconds. A value of 2 means twice that time, i.e. 10 seconds, and so on",
         // @VISIBILITY(2.5)
         openspace::properties::Property::Visibility::User
     };
@@ -137,7 +137,7 @@ PathNavigator::PathNavigator()
     , _speedScale(SpeedScaleInfo, 1.f, 0.01f, 2.f)
     , _applyIdleBehaviorOnFinish(IdleBehaviorOnFinishInfo, false)
     , _arrivalDistanceFactor(ArrivalDistanceFactorInfo, 2.0, 0.1, 20.0)
-    , _linearRotationSpeedFactor(RotationSpeedFactorInfo, 1.f, 0.1f, 2.f)
+    , _linearRotationSpeedFactor(RotationSpeedFactorInfo, 1.5f, 0.1f, 3.f)
     , _minValidBoundingSphere(MinBoundingSphereInfo, 10.0, 1.0, 3e10)
     , _relevantNodeTags(RelevantNodeTagsInfo)
 {
