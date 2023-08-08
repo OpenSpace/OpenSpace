76358e967c3e51c18fe00b646c338424a5cc9343
Malin E <malin.ejdbo@gmail.com>
2023-08-08 14:07:47 +0200

    Small clean up

--- modules/spacecraftinstruments/rendering/renderableplaneprojection.cpp
+++ modules/spacecraftinstruments/rendering/renderableplaneprojection.cpp
@@ -125,9 +125,14 @@ void RenderablePlaneProjection::render(const RenderData& data, RendererTasks&) {
     glm::dmat4 modelTransform =
         glm::translate(glm::dmat4(1.0), data.modelTransform.translation) *
         glm::dmat4(_stateMatrix);
-    const glm::mat4 mvp = calcModelViewProjectionTransform(data, modelTransform);
 
-    _shader->setUniform("modelViewProjectionTransform", mvp);
+    const glm::dmat4 ModelViewProjectionTransform =
+        calcModelViewProjectionTransform(data, modelTransform);
+
+    _shader->setUniform(
+        "modelViewProjectionTransform",
+        glm::mat4(ModelViewProjectionTransform)
+    );
 
     ghoul::opengl::TextureUnit unit;
     unit.activate();
