65a0e376918cbbc5f24c478dbacd266afb7bb7d4
Emma Broman <emma.broman@liu.se>
2023-09-11 10:56:33 +0200

    Remove a todo comment

--- modules/base/shaders/arrow_fs.glsl
+++ modules/base/shaders/arrow_fs.glsl
@@ -44,7 +44,6 @@ Fragment getFragment() {
 
   // Simple phong shading (same color for diffuse and ambient. White specular)
   if (performShading) {
-    // TODO: send from code and make configurable
     const vec3 lightDirectionViewSpace = vec3(0.0, 0.0, 1.0);
     const float lightIntensity = 1.0;
     const float specularPower = 100.0;
