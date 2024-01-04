6dd497d38c431e91a004703cbb72a2cd81f68ead
Emma Broman <emma.broman@liu.se>
2024-01-04 13:55:21 +0100

    Feature/add some docs (#2961)
    * Add docs for PathInstruction
    
    * Add docs for renderablecartesianaxes and navigationstate
    
    * Write docs for ExoplanetDataPreparationTask
    
    * Add some kind of documentation for the task base class
    
    Was already showing in the list, but the property or decription was not included
    
    * Apply suggestions from code review
    
    * Address review comments
    
    * Clarify confusing sentence
    
    * Slight rephrasing

--- src/documentation/core_registration.cpp
+++ src/documentation/core_registration.cpp
@@ -57,6 +57,7 @@
 #include <openspace/scripting/scriptscheduler.h>
 #include <openspace/scripting/systemcapabilitiesbinding.h>
 #include <openspace/util/spicemanager.h>
+#include <openspace/util/task.h>
 #include <openspace/util/time.h>
 #include <openspace/util/timerange.h>
 
@@ -74,6 +75,7 @@ void registerCoreClasses(documentation::DocumentationEngine& engine) {
     engine.addDocumentation(Scale::Documentation());
     engine.addDocumentation(SceneGraphNode::Documentation());
     engine.addDocumentation(ScreenSpaceRenderable::Documentation());
+    engine.addDocumentation(Task::documentation());
     engine.addDocumentation(TimeRange::Documentation());
     engine.addDocumentation(Translation::Documentation());
     engine.addDocumentation(TimeFrame::Documentation());
