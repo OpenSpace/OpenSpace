81ceda06c21fbbd67daa72822845079e544d19d0
Emma Broman <emma.broman@liu.se>
2024-01-04 09:53:51 +0100

    Clarify confusing sentence

--- src/navigation/navigationstate.cpp
+++ src/navigation/navigationstate.cpp
@@ -46,9 +46,11 @@ namespace {
     // `openspace.navigation.getNavigationState()` function in the Scripting API.
     //
     // Note that a NavigationState does not include information about what timestamp
-    // within OpenSpace that the NavigationState was generated. Setting the camera to
-    // a speficic state will result in different visuals if the simulation timestamp is
-    // different to that of when it was created/saved.
+    // within OpenSpace that the NavigationState was generated. The visuals may be
+    // different depending on what the simulation timestamp is when the NavigationState
+    // is loaded, as the relative positions of objects in the scene may have changed.
+    // The get the exact same visuals as when the NavigationState was saved, make sure
+    // to also set the time to be the same as on save.
     struct [[codegen::Dictionary(NavigationState)]] Parameters {
         // The identifier of the anchor node
         std::string anchor;
