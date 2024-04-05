76b99a2e01c338d51569108cd36fdf10c201f8dc
Alexander Bock <alexander.bock@liu.se>
2024-04-05 11:18:32 +0200

    Mutex-protect the time conversion in the URLSynchronization to prevent a rare crash when accessing SpiceManager multithreaded

--- modules/sync/syncs/urlsynchronization.h
+++ modules/sync/syncs/urlsynchronization.h
@@ -122,6 +122,8 @@ private:
 
     /// Determines how long the file is valid before it should be downloaded again
     double _secondsUntilResync = MaxDateAsJ2000;
+
+    std::mutex _mutex;
 };
 
 } // namespace openspace
