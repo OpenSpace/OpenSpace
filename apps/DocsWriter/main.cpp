264af1aae98851f9cf4afd44e7b730f93644923c
Ylva Selling <ylva.selling@gmail.com>
2024-05-03 11:33:18 +0200

    Update apps/DocsWriter/main.cpp
    Co-authored-by: Emma Broman <emma.broman@liu.se>

--- apps/DocsWriter/main.cpp
+++ apps/DocsWriter/main.cpp
@@ -70,7 +70,7 @@ int main(int argc, char** argv) {
     global::openSpaceEngine->initialize();
 
     // Print out the documentation to the documentation folder
-    // @TODO: ylvse-2024-05-02 change this directory when integrating with jenkins?
+    // @TODO (ylvse, 2024-05-02) change this directory when integrating with jenkins?
     DocEng.writeJsonDocumentation();
 
     return 0;
