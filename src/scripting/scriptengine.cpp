ab4023b9fd90448fb35912cab4e5fbd45fd94f50
Alexander Bock <alexander.bock@liu.se>
2024-08-14 14:38:46 +0200

    No longer send the script source of shortcuts via the topic.  Don't scripts into the log that are binary blobs

--- src/scripting/scriptengine.cpp
+++ src/scripting/scriptengine.cpp
@@ -170,8 +170,9 @@ bool ScriptEngine::runScript(const std::string& script, const ScriptCallback& ca
 
     ghoul_assert(!script.empty(), "Script must not be empty");
 
-    if (_logScripts) {
-        // Write command to log before it's executed
+    if (_logScripts && script[0] != '\x1b') {
+        // \x1b = A byte in the beginning that Lua uses to represent whether a string is
+        // an ASCII or a binary blob. We don't want to print binary blobs into the log
         writeLog(script);
     }
 
