8d25fd18821add23492d1c302011717bd6151918
Ylva Selling <ylva.selling@gmail.com>
2024-08-23 09:32:48 +0200

    Update modules/webbrowser/include/browserinstance.h
    Co-authored-by: Emma Broman <emma.broman@liu.se>

--- modules/webbrowser/include/browserinstance.h
+++ modules/webbrowser/include/browserinstance.h
@@ -57,7 +57,7 @@ class WebKeyboardHandler;
 class BrowserInstance {
 public:
     static constexpr int SingleClick = 1;
-    // TODO: ylvse 2024-08-20: remove third argument when the sky browser rewrite is done.
+    // @TODO (ylvse 2024-08-20): remove third argument when the sky browser rewrite is done.
     //   The browser instance should always accelerate the rendering if possible but for
     //   now the skybrowser is not accelerated. Will be when the rewrite is done.
     BrowserInstance(WebRenderHandler* renderer,
