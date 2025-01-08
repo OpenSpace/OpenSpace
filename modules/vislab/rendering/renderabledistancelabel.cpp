3edd4a447598bf733487238e096603c2ebb14204
Emma Broman <emma.broman@liu.se>
2025-01-08 11:00:33 +0100

    Update add add documentation for `RenderableDistanceLabel`

--- modules/vislab/rendering/renderabledistancelabel.cpp
+++ modules/vislab/rendering/renderabledistancelabel.cpp
@@ -61,12 +61,20 @@ namespace {
         "CustomUnitDescriptor",
         "Custom Unit Descriptor",
         "Property to define a custom unit descriptor to use to describe the distance "
-        "value. Defaults to the units SI descriptor if not specified.",
+        "value. Defaults to the selected unit's SI descriptor if not specified.",
         openspace::properties::Property::Visibility::AdvancedUser
     };
 
+    // This `Renderable` creates a label that shows the distance between two nodes, based
+    // on an existing [RenderableNodeLine](#base_renderable_renderablenodeline). The label
+    // will be placed halfway between the two scene graph nodes that the line connects.
+    //
+    // The unit in which the distance is displayed can be customized.
     struct [[codegen::Dictionary(RenderableDistanceLabel)]] Parameters {
-        // [[codegen::verbatim(NodeLineInfo.description)]]
+        // The identifier of a scene graph node with a
+        // [RenderableNodeLine](#base_renderable_renderablenodeline) that this label
+        // should track. The label text will be updating based on the distance from the
+        // node line's start and end.
         std::string nodeLine;
 
         // [[codegen::verbatim(DistanceUnitInfo.description)]]
