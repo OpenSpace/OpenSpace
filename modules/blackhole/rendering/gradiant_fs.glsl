605a26425eeabc43b46a0019d2096c20f51c5f90
Emil Wallberg <emil.wallberg@proton.me>
2025-02-17 16:25:20 +0100

    Texture rendering
    Rendering texture to the camera quad
    
    Co-Authored-By: Wilhelm Björkström <143391787+Grantallkotten@users.noreply.github.com>

--- modules/blackhole/rendering/gradiant_fs.glsl
+++
@@ -1,9 +0,0 @@
-#include "fragment.glsl"
-in vec2 TexCoord;
-
-Fragment getFragment() {
-    Fragment frag;
-    // Gradient from blue (bottom-left) to purple (top-right)
-    frag.color = vec4(TexCoord.x, TexCoord.y, 1.0 - TexCoord.x, 1.0);
-    return frag;
-}
