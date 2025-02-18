a9beb476afd9d31608b28730782dc653cf31b3ad
Andreas Engberg <adde.engberg@gmail.com>
2025-02-18 12:01:13 +0100

    added optional parameter to overwrite session recording file

--- include/openspace/interaction/sessionrecordinghandler.h
+++ include/openspace/interaction/sessionrecordinghandler.h
@@ -106,7 +106,7 @@ public:
      * and all keyframes deleted from memory.
      * \param filename File saved with recorded keyframes
      */
-    void stopRecording(const std::filesystem::path& filename, DataMode dataMode);
+    void stopRecording(const std::filesystem::path& filename, DataMode dataMode, bool overwrite = false);
 
     /**
      * Used to check if a session recording is in progress.
