65b89c2c8bb1f4ced6960f5a83fd4c671f241a9a
Wilhelm Björkström <143391787+Grantallkotten@users.noreply.github.com>
2025-02-27 17:32:22 +0100

    Environment map from spherical coordinates.
    Co-Authored-By: Emil Wallberg <49481622+EmilWallberg@users.noreply.github.com>

--- modules/blackhole/rendering/viewport.cpp
+++ modules/blackhole/rendering/viewport.cpp
@@ -1,7 +1,14 @@
 #include <modules/blackhole/rendering/viewport.h>
 
-#include <glm/gtc/constants.hpp>
-#include <glm/gtc/matrix_transform.hpp>
+#include <ghoul/opengl/texture.h>
+#include <ghoul/logging/logmanager.h>
+
+#ifndef PI
+#define PI 3.1415926535897932384626433832795f
+#endif
+namespace {
+    constexpr std::string_view _loggerCat = "BlackHoleViewPort";
+}
 
 namespace openspace {
 
@@ -19,4 +26,49 @@ namespace openspace {
 
         return glm::dvec3(r, theta, phi);
     }
+
+    void ViewPort::uploadViewGrid(const glm::ivec2& screenSize) {
+        float halfWidth = tan(fovHorizontel / 2.f);
+        float halfHeight = halfWidth * screenSize.y / screenSize.x;
+
+        float stepSize = (2.f * halfWidth) / screenSize.x;
+
+        std::vector<float> data(screenSize.x * screenSize.y * 2, 0.0f);
+
+        float z = -1.0f; 
+        for (int i = 0; i < screenSize.y; i++) {
+            float y = (i - screenSize.y / 2) * stepSize;
+            for (int j = 0; j < screenSize.x; j++) {
+                float x = (j - screenSize.x / 2) * stepSize;
+
+                float theta = atan2(sqrt(x * x + y * y) , z);
+                float phi = atan2(y, x);
+
+                int index = (screenSize.x * i + j) * 2;
+                data[index] = phi;
+                data[index + 1] = theta;
+            }
+        }
+        updateViewGridTexture(data, screenSize);
+    }
+
+    void ViewPort::updateViewGridTexture(std::vector<float>& data, const glm::vec2& screenSize) {
+        viewGrid = std::make_unique<ghoul::opengl::Texture>(
+            data.data(),
+            glm::uvec3(screenSize.x, screenSize.y, 1),
+            GL_TEXTURE_2D,
+            ghoul::opengl::Texture::Format::RG,
+            GL_RG32F,
+            GL_FLOAT
+        );
+
+        if (viewGrid) {
+            viewGrid->uploadTexture();
+        }
+        else {
+            LWARNING("Failed to load view grid on update");
+        }
+    }
+
+
 } // openspace namespace
--- modules/blackhole/rendering/viewport.h
+++ modules/blackhole/rendering/viewport.h
@@ -2,6 +2,8 @@
 #define __OPENSPACE_MODULE_BLACKHOLE___BLACKHOLECAMERA___H__
 
 #include <openspace/camera/camera.h>
+#include <ghoul/opengl/uniformcache.h>
+#include <ghoul/opengl/texture.h>
 #include <glm/glm.hpp>
 
 namespace openspace {
@@ -11,9 +13,12 @@ namespace openspace {
         ViewPort(Camera* camera);
 
         glm::dvec3 sphericalPosition() const;
-
+        void uploadViewGrid(const glm::ivec2& screenSize);
+        void updateViewGridTexture(std::vector<float>& data, const glm::vec2& screenSize);
+        std::unique_ptr<ghoul::opengl::Texture> viewGrid;
     private:
         Camera* _camera = nullptr;
+        float fovHorizontel = glm::radians(120.f);
     };
 } // namespace openspace
 
