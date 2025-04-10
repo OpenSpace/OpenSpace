617fc81d0a0c6ba3f81e68b3df274dcf1a6d79a1
Alexander Bock <mail@alexanderbock.eu>
2025-04-10 13:15:20 +0200

    Only use colorscheme if it is available

--- apps/OpenSpace/ext/launcher/src/notificationwindow.cpp
+++ apps/OpenSpace/ext/launcher/src/notificationwindow.cpp
@@ -85,6 +85,7 @@ namespace {
             date::day(day)
         );
 
+#if (QT_VERSION >= QT_VERSION_CHECK(6, 5, 0))
         Qt::ColorScheme scheme = QGuiApplication::styleHints()->colorScheme();
         QColor text = QGuiApplication::palette().text().color();
         if (scheme == Qt::ColorScheme::Dark) {
@@ -93,6 +94,10 @@ namespace {
         else {
             text = text.lighter();
         }
+#else
+        QColor text = QGuiApplication::palette().text().color();
+        text = text.darker();
+#endif
 
         if (date::sys_days(ymd) < date::sys_days(lastStartedDate)) {
             return std::format(
