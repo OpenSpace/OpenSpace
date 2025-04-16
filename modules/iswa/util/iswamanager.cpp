125f54aa0aca68912815408234aa035e2932b6a3
Alexander Bock <alexander.bock@liu.se>
2025-04-16 14:13:20 +0200

    Update nlohmann repository (#3609)
    * Remove old non-submodule version of nlohmann in favor of submodule
    * Update nlohmann from 3.9.1 to 3.12.0
    - Already disable implicit conversions in preparation for next major version

--- modules/iswa/util/iswamanager.cpp
+++ modules/iswa/util/iswamanager.cpp
@@ -356,16 +356,24 @@ std::string IswaManager::jsonPlaneToLuaTable(MetadataFuture& data) {
     }
     nlohmann::json j = nlohmann::json::parse(data.json);
 
-    std::string parent = j["Central Body"];
-    std::string frame = j["Coordinates"];
-    std::string coordinateType = j["Coordinate Type"];
-    int updateTime = j["ISWA_UPDATE_SECONDS"];
+    std::string parent = j["Central Body"].get<std::string>();
+    std::string frame = j["Coordinates"].get<std::string>();
+    std::string coordinateType = j["Coordinate Type"].get<std::string>();
+    int updateTime = j["ISWA_UPDATE_SECONDS"].get<int>();
 
-    glm::vec3 max(j["Plot XMAX"], j["Plot YMAX"], j["Plot ZMAX"]);
-    glm::vec3 min(j["Plot XMIN"], j["Plot YMIN"], j["Plot ZMIN"]);
+    glm::vec3 max = glm::vec3(
+        j["Plot XMAX"].get<float>(),
+        j["Plot YMAX"].get<float>(),
+        j["Plot ZMAX"].get<float>()
+    );
+    glm::vec3 min = glm::vec3(
+        j["Plot XMIN"].get<float>(),
+        j["Plot YMIN"].get<float>(),
+        j["Plot ZMIN"].get<float>()
+    );
 
     glm::vec4 spatialScale(1.f, 1.f, 1.f, 10.f);
-    std::string spatial = j["Spatial Scale (Custom)"];
+    std::string spatial = j["Spatial Scale (Custom)"].get<std::string>();
     if (spatial == "R_E") {
         spatialScale.x = 6.371f;
         spatialScale.y = 6.371f;
@@ -472,12 +480,12 @@ std::string IswaManager::jsonSphereToLuaTable(MetadataFuture& data) {
 
     nlohmann::json j = nlohmann::json::parse(data.json);
     j = j["metadata"];
-    std::string parent = j["central_body"];
+    std::string parent = j["central_body"].get<std::string>();
     parent[0] = static_cast<char>(toupper(static_cast<int>(parent[0])));
-    std::string frame = j["standard_grid_target"];
-    std::string coordinateType = j["grid_1_type"];
-    float updateTime = j["output_time_interval"];
-    float radius = j["radius"];
+    std::string frame = j["standard_grid_target"].get<std::string>();
+    std::string coordinateType = j["grid_1_type"].get<std::string>();
+    float updateTime = j["output_time_interval"].get<float>();
+    float radius = j["radius"].get<float>();
 
     glm::vec3 max(
         j["x"]["actual_max"],
@@ -667,18 +675,17 @@ void IswaManager::fillCygnetInfo(std::string jsonString) {
             for (size_t i = 0; i < jsonList.size(); i++) {
                 nlohmann::json jCygnet = jsonList.at(i);
 
-                std::string name = jCygnet["cygnetDisplayTitle"];
+                std::string name = jCygnet["cygnetDisplayTitle"].get<std::string>();
                 std::replace(name.begin(), name.end(),'.', ',');
 
                 CygnetInfo info = {
                     name,
-                    jCygnet["cygnetDescription"],
-                    jCygnet["cygnetUpdateInterval"],
+                    jCygnet["cygnetDescription"].get<std::string>(),
+                    jCygnet["cygnetUpdateInterval"].get<int>(),
                     false
                 };
-                _cygnetInformation[jCygnet["cygnetID"]] = std::make_shared<CygnetInfo>(
-                    info
-                );
+                _cygnetInformation[jCygnet["cygnetID"].get<int>()] =
+                    std::make_shared<CygnetInfo>(info);
             }
         }
     }
@@ -699,8 +706,9 @@ void IswaManager::addCdfFiles(std::string cdfpath) {
             for (size_t i = 0; i < cdfGroups.size(); i++) {
                 nlohmann::json cdfGroup = cdfGroups.at(i);
 
-                std::string groupName = cdfGroup["group"];
-                std::string fieldlineSeedsIndexFile = cdfGroup["fieldlinefile"];
+                std::string groupName = cdfGroup["group"].get<std::string>();
+                std::string fieldlineSeedsIndexFile =
+                    cdfGroup["fieldlinefile"].get<std::string>();
 
                 if (_cdfInformation.find(groupName) != _cdfInformation.end()) {
                     LWARNING("CdfGroup with name" + groupName + " already exists");
@@ -713,9 +721,9 @@ void IswaManager::addCdfFiles(std::string cdfpath) {
                 for (size_t j = 0; j < cdfs.size(); j++) {
                     nlohmann::json cdf = cdfs.at(j);
 
-                    std::string name = cdf["name"];
-                    std::string path = cdf["path"];
-                    std::string date = cdf["date"];
+                    std::string name = cdf["name"].get<std::string>();
+                    std::string path = cdf["path"].get<std::string>();
+                    std::string date = cdf["date"].get<std::string>();
 
                     _cdfInformation[groupName].push_back({
                         name,
