56bc4cdc4c05cf7d7d116e75909984f2f6bfd21c
hn-88 <hn-88@users.noreply.github.com>
2025-06-02 13:37:56 +0530

    Update modules/webbrowser/cmake/patch/cmake/cef_variables.cmake
    Co-authored-by: Alexander Bock <mail@alexanderbock.eu>

--- modules/webbrowser/cmake/patch/cmake/cef_variables.cmake
+++ modules/webbrowser/cmake/patch/cmake/cef_variables.cmake
@@ -339,8 +339,7 @@ if(OS_MACOSX)
       set(CEF_BINARY_DIR "${_CEF_ROOT}/$<CONFIG>")
   endif()
 
-  message("CMAKE_OSX_DEPLOYMENT_TARGET is set to ")
-  message(${CMAKE_OSX_DEPLOYMENT_TARGET})
+  message("CMAKE_OSX_DEPLOYMENT_TARGET is set to ${CMAKE_OSX_DEPLOYMENT_TARGET}")
 
   set(CEF_BINARY_DIR_DEBUG    "${_CEF_ROOT}/Debug")
   set(CEF_BINARY_DIR_RELEASE  "${_CEF_ROOT}/Release")
