85959d9ce5b55fba6f31f7963723acb8da7f80fc
Joakim Kilby <jockekilby@gmail.com>
2025-08-29 07:31:12 +0200

    fix issue with rendering of planetary-trails

--- modules/globebrowsing/shaders/texturetilemapping.glsl
+++ modules/globebrowsing/shaders/texturetilemapping.glsl
@@ -164,9 +164,9 @@ vec4 getSample#{layerGroup}#{i}(vec2 uv, vec3 levelWeights,
   c = getTexVal(#{layerGroup}[#{i}].pile, levelWeights, uv);
 #elif (#{#{layerGroup}#{i}LayerType} == 11) // VideoTileProvider
   c = getTexVal(#{layerGroup}[#{i}].pile, levelWeights, uv);
-#elif (#{#{layerGroup}#{i}LayerType} == 11) // PlanetaryTrailTileProvider
+#elif (#{#{layerGroup}#{i}LayerType} == 12) // PlanetaryTrailTileProvider
   c = getTexVal(#{layerGroup}[#{i}].pile, levelWeights, uv);
-#elif (#{#{layerGroup}#{i}LayerType} == 12) // ForceHighResolutionTileProvider
+#elif (#{#{layerGroup}#{i}LayerType} == 13) // ForceHighResolutionTileProvider
   // does nothing but uses variables to avoid compilation warnings....
   LayerSettings settings = #{layerGroup}[#{i}].settings;
   c *= settings.gamma * settings.multiplier * min(settings.opacity, 0);
