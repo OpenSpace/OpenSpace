7cb4f23158288cb28668f218f5008bc9858cf813
Emma Broman <emma.broman@liu.se>
2025-12-01 14:54:24 +0100

    Move texture mapping to fragment shader
    Fixes issues at edges due to interpolation of texture coordinates

--- modules/base/shaders/sphere_vs.glsl
+++ modules/base/shaders/sphere_vs.glsl
@@ -36,49 +36,8 @@ uniform mat4 modelViewProjection;
 uniform mat4 modelViewTransform;
 uniform mat3 modelViewRotation;
 
-const int Equirectangular = 0;
-const int AngularFisheye = 1;
-uniform int textureProjection;
-
-const float M_PI = 3.14159265358979323846;
-
-// Remap equirectangular texture coordinates into angular fisheye
-vec2 equiToAngularFisheye(vec2 textureCoords) {
-  vec2 pos2 = textureCoords * 2.0 - 1.0; // Map [0,1] tex coords to [-1,1]
-
-  // 2D equi to 3D vector
-  float lat = pos2.y * 0.5 * M_PI;
-  float lon = pos2.x * M_PI;
-
-  // Map to 3D position, with Z being the north pole
-  vec3 pos3 = vec3(
-    cos(lat) * cos(lon),
-    sin(lat),
-    cos(lat) * sin(lon)
-  );
-
-  float coverAngle = M_PI; // 180 degrees
-
-  // 3D vector to normalized 2D fisheye [-1,1]
-  float r = 2.0 / coverAngle * atan(sqrt(dot(pos3.xz, pos3.xz)), pos3.y);
-  float theta = atan(pos3.z, pos3.x);
-  vec2 fisheye2D = vec2(r * cos(theta), r * sin(theta));
-
-  if (r > 1.0) {
-    return vec2(-1.0, -1.0); // Indicate invalid coordinates
-  }
-
-  // Remap to [0,1]
-  return 0.5 * fisheye2D + 0.5;
-}
-
 void main() {
-  if (textureProjection == AngularFisheye) {
-    vs_textureCoords = equiToAngularFisheye(in_textureCoords);
-  }
-  else { // Equirectangular
-    vs_textureCoords = in_textureCoords;
-  }
+  vs_textureCoords = in_textureCoords;
 
   vs_normal = modelViewRotation * normalize(in_position.xyz);
   vec4 position = modelViewProjection * vec4(in_position.xyz, 1.0);
