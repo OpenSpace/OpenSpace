20b1a5ccfe7acee8f286cbc01566399f1908a177
Matthias Braun <30432154+braunms@users.noreply.github.com>
2025-12-03 18:35:58 +0100

    Make the profile editor (launcher GUI) and thus the Qt6 dependency optional (#3857)

--- apps/OpenSpace/main.cpp
+++ apps/OpenSpace/main.cpp
@@ -79,9 +79,11 @@
 #include <float.h>
 #endif // OPENSPACE_BREAK_ON_FLOATING_POINT_EXCEPTION
 
+#ifdef OPENSPACE_HAS_LAUNCHER
 #include <launcherwindow.h>
 #include <QApplication>
 #include <QMessageBox>
+#endif // OPENSPACE_HAS_LAUNCHER
 
 #ifdef WIN32
 extern "C" {
@@ -1450,6 +1452,7 @@ int main(int argc, char* argv[]) {
 #endif // __APPLE__
 
     if (!global::configuration->bypassLauncher) {
+#ifdef OPENSPACE_HAS_LAUNCHER
 #ifndef __APPLE__
         int qac = 0;
         QApplication app(qac, nullptr);
@@ -1542,6 +1545,9 @@ int main(int argc, char* argv[]) {
             }
             global::configuration->windowConfiguration = config;
         }
+#else // ^^^^ OPENSPACE_HAS_LAUNCHER // !OPENSPACE_HAS_LAUNCHER
+        glfwInit();
+#endif // OPENSPACE_HAS_LAUNCHER
     }
     else {
         glfwInit();
