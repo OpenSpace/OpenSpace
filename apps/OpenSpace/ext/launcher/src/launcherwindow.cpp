6237ea5560eb2d53cc486e19eb3ae895a28e88d5
Mathis Brossier <mathis.brossier@gmail.com>
2025-12-08 13:46:16 +0100

    launcher: use generic_string to display paths (platform independent path)

--- apps/OpenSpace/ext/launcher/src/launcherwindow.cpp
+++ apps/OpenSpace/ext/launcher/src/launcherwindow.cpp
@@ -166,7 +166,7 @@ LauncherWindow::LauncherWindow(bool profileEnabled, const Configuration& globalC
             centralWidget
         );
     }
-    
+
     {
         QLabel* logoImage = new QLabel(centralWidget);
         logoImage->setObjectName("clear");
@@ -323,7 +323,7 @@ LauncherWindow::LauncherWindow(bool profileEnabled, const Configuration& globalC
     connect(
         _windowConfigBox, &SplitComboBox::selectionChanged,
         this, &LauncherWindow::selectConfiguration
-    ); 
+    );
     connect(
         _windowConfigBox, &SplitComboBox::selectionChanged,
         this, &LauncherWindow::updateStartButton
