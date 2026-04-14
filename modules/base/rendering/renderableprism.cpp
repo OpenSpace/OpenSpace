f265b65d8c0c8252280b443f9ba9b72a5571f924
Emma Broman <emma.broman@liu.se>
2026-04-14 11:29:29 +0200

    Add docs for `RenderablePrism`

--- modules/base/rendering/renderableprism.cpp
+++ modules/base/rendering/renderableprism.cpp
@@ -92,6 +92,11 @@ namespace {
         Property::Visibility::User
     };
 
+    /**
+     * This `Renderable` renders a prism shape defined by two polygonal shapes (base and
+     * top) connected by lines. The order the polygons is defined is determined by the
+     * `Segments` property, with the same number being used for both the base and the top.
+     */
     struct [[codegen::Dictionary(RenderablePrism)]] Parameters {
         // [[codegen::verbatim(SegmentsInfo.description)]]
         int segments;
