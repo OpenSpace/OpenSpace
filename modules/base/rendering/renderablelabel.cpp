ea822b3426b9616e88f9259f45bc824035df12b2
Emma Broman <emma.broman@liu.se>
2026-04-14 11:05:25 +0200

    Add docs for `RenderableLabel`

--- modules/base/rendering/renderablelabel.cpp
+++ modules/base/rendering/renderablelabel.cpp
@@ -170,6 +170,13 @@ namespace {
         Property::Visibility::AdvancedUser
     };
 
+    /**
+     * This `Renderable` is used to render a single text label in 3D space. The label
+     * is rendered as a billboard, that is, it is always oriented to face the camera.
+     *
+     * The label can be rendered with a fade-in effect based on the distance from the
+     * camera.
+     */
     struct [[codegen::Dictionary(RenderableLabel)]] Parameters {
         enum class [[codegen::map(BlendMode)]] BlendMode {
             Normal,
