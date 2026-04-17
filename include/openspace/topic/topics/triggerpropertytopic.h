ec9911703fe35c9636155b17f281333be224a44e
Andreas Engberg <adde.engberg@gmail.com>
2026-04-17 10:14:18 +0200

    add triggerpropertytopic

--- include/openspace/topic/topics/triggerpropertytopic.h
+++ include/openspace/topic/topics/triggerpropertytopic.h
@@ -29,6 +29,8 @@
 
 namespace openspace {
 
+struct Schema;
+
 class TriggerPropertyTopic : public Topic {
 public:
     TriggerPropertyTopic() = default;
@@ -36,6 +38,8 @@ public:
 
     void handleJson(const nlohmann::json& json) override;
     bool isDone() const override;
+
+    static openspace::Schema Schema();
 };
 
 } // namespace
