8de87c16e3ad33036eeed812ef300398f7434707
Andreas Engberg <adde.engberg@gmail.com>
2026-04-17 09:50:59 +0200

    add sessionrecordingtopic
    Breaking change:
    - "refresh" event removed as it did not explicitly do anything. Previously when passing any event that is not "start_subscription" or "stop_subscription" would automatically send back the data.
    Do we want to bring back an explicit "refresh" event?

--- include/openspace/topic/topics/sessionrecordingtopic.h
+++ include/openspace/topic/topics/sessionrecordingtopic.h
@@ -31,6 +31,8 @@
 
 namespace openspace {
 
+struct Schema;
+
 class SessionRecordingTopic : public Topic {
 public:
     SessionRecordingTopic();
@@ -39,6 +41,7 @@ public:
     void handleJson(const nlohmann::json& json) override;
     bool isDone() const override;
 
+    static openspace::Schema Schema();
 private:
     static constexpr int UnsetOnChangeHandle = -1;
 
