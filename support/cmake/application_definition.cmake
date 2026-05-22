c2e72f71953266db3866893f6c6191bcece1a4b7
Alexander Bock <mail@alexanderbock.eu>
2026-05-22 14:36:02 +0200

    Attempt to fix dependency copying issue

--- support/cmake/application_definition.cmake
+++ support/cmake/application_definition.cmake
@@ -24,35 +24,33 @@
 
 include(${PROJECT_SOURCE_DIR}/support/cmake/global_variables.cmake)
 
-function (copy_files target)
-  # Add the copy command
-  foreach (file_i ${ARGN})
-    if (IS_DIRECTORY "${file_i}")
-      # copy_if_different doesn't handle directories well and just copies them without
-      # contents.  So if the path is a directory, we need to issue a different copy
-      # command
-      get_filename_component(folder ${file_i} NAME)
-      add_custom_command(
-        TARGET ${target}
-        POST_BUILD
-        COMMAND ${CMAKE_COMMAND} -E copy_directory "${file_i}" "$<TARGET_FILE_DIR:${target}>/${folder}"
-      )
-    else ()
-      add_custom_command(
-        TARGET ${target}
-        POST_BUILD
-        COMMAND ${CMAKE_COMMAND} -E copy_if_different "${file_i}" $<TARGET_FILE_DIR:${target}>
-      )
-    endif ()
-  endforeach ()
-endfunction ()
-
 function (create_new_application application_name)
   add_executable(${application_name} MACOSX_BUNDLE ${ARGN})
   set_openspace_compile_settings(${application_name})
   if (WIN32)
     get_external_library_dependencies(ext_lib)
-    copy_files(${application_name} ${ext_lib})
+    # Register one global copy target so copy commands are defined exactly once
+    if (NOT TARGET openspace_copy_external_dependencies)
+      add_custom_target(openspace_copy_external_dependencies ALL)
+      foreach (file_i ${ext_lib})
+        if (IS_DIRECTORY "${file_i}")
+          get_filename_component(folder ${file_i} NAME)
+          add_custom_command(
+            TARGET openspace_copy_external_dependencies
+            POST_BUILD
+            COMMAND ${CMAKE_COMMAND} -E copy_directory "${file_i}" "$<TARGET_FILE_DIR:${application_name}>/${folder}"
+          )
+        else ()
+          add_custom_command(
+            TARGET openspace_copy_external_dependencies
+            POST_BUILD
+            COMMAND ${CMAKE_COMMAND} -E copy_if_different "${file_i}" $<TARGET_FILE_DIR:${application_name}>
+          )
+        endif ()
+      endforeach ()
+    endif ()
+
+    add_dependencies(${application_name} openspace_copy_external_dependencies)
   endif ()
 
   target_link_libraries(${application_name} PUBLIC openspace-module-base)
