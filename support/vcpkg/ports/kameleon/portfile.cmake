e800eb26d693ae1d90305967dd7b9333dfa867ac
Alexander Bock <alexander.bock@liu.se>
2026-09-17 15:53:29 +0200

    Disable parallel build in Kameleon to fix race condition

--- support/vcpkg/ports/kameleon/portfile.cmake
+++ support/vcpkg/ports/kameleon/portfile.cmake
@@ -25,6 +25,7 @@ vcpkg_from_github(
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    DISABLE_PARALLEL_CONFIGURE
     OPTIONS
         -DKAMELEON_LIBRARY_ONLY=ON
         -DKAMELEON_USE_HDF5=OFF
